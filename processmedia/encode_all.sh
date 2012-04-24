c10cd5146a17aca48239667be86d011da0f6b688
Shish <shish@shishnet.org>
2012-04-24 22:19:05 +0100

    if there's no original video, look for original audio and video and create a video

--- processmedia/encode_all.sh
+++ processmedia/encode_all.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-for orig in files/*/original.* ; do
-	./encode.sh "$orig"
+for SONG_DIR in files/* ; do
+	if [ -d "$SONG_DIR" ] ; then
+		ORIGINAL_VIDEO=`ls $SONG_DIR/original.* | grep -E "original.(flv|mp4|avi|mkv)"`
+		if [ "x$ORIGINAL_VIDEO" = "x" ] ; then
+			echo "No original video found; looking for original audio / image"
+			IMAGE=`ls $SONG_DIR/original.* | grep -E "original.(jpg|png)"`
+			AUDIO=`ls $SONG_DIR/original.* | grep -E "original.(mp3|ogg)"`
+			AUDIO_LEN=`avconv -i $AUDIO 2>&1 | grep -oE "Duration: [0-9:\.]+" | cut -d " " -f 2` #[\d:\.]+"`
+			ORIGINAL_VIDEO=$SONG_DIR/original.mkv
+			if [ "x$IMAGE" = "x"  -o "x$AUDIO" = "x" ] ; then
+				echo "ERROR: $SONG_DIR doesn't contain an original video, nor original audio + image"
+			else
+				avconv -i $AUDIO -loop 1 -r 5 -i $IMAGE -t $AUDIO_LEN -vf scale=640:-1 -acodec copy $ORIGINAL_VIDEO
+			fi
+		fi
+		#echo Original: $ORIGINAL_VIDEO
+		./encode.sh $ORIGINAL_VIDEO
+	fi
 done
