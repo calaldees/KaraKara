781b1cc49f2240156933cebaffb2ee854c89f761
Carl Ritson <critson@perlfu.co.uk>
2012-07-17 16:50:57 +0100

    Add srt to media file regular expression.

--- processmedia/processmedia.py
+++ processmedia/processmedia.py
@@ -50,7 +50,7 @@ def hidden_file_re():
 	return re.compile(r'^\..*$')
 
 def media_file_re():
-	return re.compile(r'^.*\.(avi|aac|mp3|mp4|mpg|mpeg|mkv|ogg|ogm|rm|wav|wmv|ass|ssa|bmp|png|jpg|jpeg)$', re.IGNORECASE)
+	return re.compile(r'^.*\.(avi|aac|mp3|mp4|mpg|mpeg|mkv|ogg|ogm|rm|wav|wmv|ass|ssa|srt|bmp|png|jpg|jpeg)$', re.IGNORECASE)
 
 def video_file_re():
 	return re.compile(r'^.*\.(avi|mp4|mpg|mpeg|mkv|rm|ogm|wmv)$', re.IGNORECASE)
