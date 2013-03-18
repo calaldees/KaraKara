94e5869417394fdfad3b1828771afb0f7aee5366
Allan Callaghan <calaldees@hotmail.com>
2013-03-18 19:33:58 +0000

    enhanced track tests, starting to test other formats

--- website/karakara/tests/integration/test_tracks.py
+++ website/karakara/tests/integration/test_tracks.py
@@ -2,14 +2,31 @@
 
 import pytest
 
+test_formats = ['html'] #, 'json', 'xml'
+
+def pytest_generate_tests (metafunc):
+    if 'format' in metafunc.fixturenames:
+        metafunc.parametrize('format', test_formats)
 @pytest.mark.parametrize(('track_id', 'expected_response', 'text_list',), [
-    ('t1', 200, ['Test track 1'   , 'series x', 'anime', 'ここ', 'test/image1.jpg'  ]),
-    ('t2', 200, ['Test track 2'   , 'series x', 'anime', 'äöü', 'test/preview2.flv']),
-    ('t3', 200, ['Test track 3 キ', 'series y', 'anime',        'test/preview3.mp4']),
+    ('t1', 200, ['Test Track 1'   , 'series X', 'anime', 'ここ', 'test/image1.jpg'  ]),
+    ('t2', 200, ['Test Track 2'   , 'series X', 'anime', 'äöü', 'test/preview2.flv']),
+    ('t3', 200, ['Test Track 3 キ', 'series Y', 'anime',        'test/preview3.mp4']),
     #('t4', 404),
 ])
-def test_track_view(app, tracks, track_id, expected_response, text_list):
-    response = app.get('/track/{0}'.format(track_id))
+def test_track_view(app, tracks, track_id, format, expected_response, text_list):
+    """
+    Test the track view
+    """
+    url = '/track/{0}'.format(track_id)
+    if format:
+        url += '?format='+format
+    response = app.get(url)
     assert response.status_code == expected_response
     for text in text_list:
+        assert text.lower() in response.text.lower()
+
+def test_track_list_all(app, tracks):
+    response = app.get('/track_list')
+    for text in ['track 1', 'track 2', 'track 3']:
         assert text in response.text
+
