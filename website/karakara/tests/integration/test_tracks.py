be0bba806e7ee79840bcd3e49464810db66ab80b
Allan Callaghan <calaldees@hotmail.com>
2013-03-19 07:28:21 +0000

    stable track view test that test api output as well (still a few assertions to sort)

--- website/karakara/tests/integration/test_tracks.py
+++ website/karakara/tests/integration/test_tracks.py
@@ -2,31 +2,37 @@
 
 import pytest
 
-test_formats = ['html'] #, 'json', 'xml'
-
-def pytest_generate_tests (metafunc):
-    if 'format' in metafunc.fixturenames:
-        metafunc.parametrize('format', test_formats)
 @pytest.mark.parametrize(('track_id', 'expected_response', 'text_list',), [
     ('t1', 200, ['Test Track 1'   , 'series X', 'anime', 'ここ', 'test/image1.jpg'  ]),
     ('t2', 200, ['Test Track 2'   , 'series X', 'anime', 'äöü', 'test/preview2.flv']),
     ('t3', 200, ['Test Track 3 キ', 'series Y', 'anime',        'test/preview3.mp4']),
     #('t4', 404),
 ])
-def test_track_view(app, tracks, track_id, format, expected_response, text_list):
+def test_track_view(app, tracks, track_id, expected_response, text_list):
     """
-    Test the track view
+    Test the track html view
+    Assert the track data is rendered by the template 
     """
     url = '/track/{0}'.format(track_id)
-    if format:
-        url += '?format='+format
     response = app.get(url)
     assert response.status_code == expected_response
     for text in text_list:
         assert text.lower() in response.text.lower()
 
+def test_track_view_api(app, tracks):
+    """
+    Track's can be returned as structured JSON data
+    Lightly touch the structure and test correct unicode throughput
+    """
+    data = app.get('/track/t1?format=json').json['data']
+    assert data['track']['id'] == 't1'
+    assert 'ここ' in data['track']['lyrics'][0]['content']
+    
 def test_track_list_all(app, tracks):
     response = app.get('/track_list')
     for text in ['track 1', 'track 2', 'track 3']:
         assert text in response.text
 
+def test_track_list_all_api(app, tracks):
+    data = app.get('/track_list?format=json').json['data']
+    #assert 'test track 2' in [title for title in track['tags']['title'] for track in data['list']]
