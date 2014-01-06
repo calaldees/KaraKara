b8c1b6f423a7d1d3156736bddab6ae7ab5f396bf
Allan Callaghan <calaldees@hotmail.com>
2014-01-06 19:54:38 +0000

    Completed 'Remote Control' tests with multiprocess lib

--- website/karakara/tests/integration/test_remote.py
+++ website/karakara/tests/integration/test_remote.py
@@ -1,33 +1,9 @@
-from karakara.tests.conftest import unfinished
-
 import re
 from bs4 import BeautifulSoup
-import socket, threading
+import socket
 
-class SocketClientTest():
-    connected = True
-    last_message = ''
+from multiprocessing import Process, Queue
 
-    def __init__(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("localhost", 9872))
-        thread = threading.Thread(target=self.connection, args=(sock,))
-        thread.daemon=True
-        thread.start()
-
-    def connection(self, sock):
-        print("connected: test")
-        while self.connected:
-            data_recv = sock.recv(4098)
-            if not data_recv:
-                break
-            print("message recived: {0}".format(data_recv))
-            self.last_message = data_recv
-        print("disconnected: thats all folks")
-        sock.close()
-
-
-@unfinished
 def test_remote_control(app):
     """
     Connect to socketserver - unfortunately with a plain tcp socket rather than a websocket
@@ -35,18 +11,42 @@ def test_remote_control(app):
     Press remote buttons
     Assert socket receives the correct message
     """
-    client = SocketClientTest()
+    # Non admins cannot use the remote control
+    response = app.get('/remote', expect_errors=True)
+    response.status_code == 403
+    
+    # Attain admin privilages
+    response_json = app.get('/admin.json').json
+    assert response_json['identity']['admin']
+
+    def connection(sock, message_received_queue):
+        while True:
+            data_recv = sock.recv(4098)
+            if not data_recv:
+                break
+            message_received_queue.put(data_recv.decode('utf-8'))
+        sock.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("localhost", 9872))
+    message_received_queue = Queue()
+    client_listener_process = Process(target=connection, args=(sock, message_received_queue,))
+    client_listener_process.daemon = True
+    client_listener_process.start()
     
     def press_button(soup, button_text):
         url = soup.find('a', text=re.compile(button_text, flags=re.IGNORECASE))['href']
-        print(url)
         response = app.get('/remote{0}'.format(url))
         assert response.status_code==200
         assert 'remote' in response.text.lower()
-        assert client.last_message == button_text
+        assert button_text in message_received_queue.get(timeout=1)
 
     soup = BeautifulSoup(app.get('/remote').text)
     for button_name in ('play', 'pause', 'seek', 'stop', 'skip'):
         press_button(soup, button_name)
 
-    client.connected = False
+    client_listener_process.terminate()
+    client_listener_process.join()
+    
+    # Revolk admin privilages
+    response_json = app.get('/admin.json').json
+    assert not response_json['identity']['admin']
