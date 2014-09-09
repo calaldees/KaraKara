38e847af760c7be5e5fd191a4fc4b2772acd5a78
Allan Callaghan <calaldees@gmail.com>
2014-09-09 07:32:51 +0100

    Removed unnneded monkeypatch pep8'ed (may have to do this for some of the rest of the codebase (grrrr)

--- website/karakara/__init__.py
+++ website/karakara/__init__.py
@@ -23,26 +23,22 @@ from pyramid.session import SignedCookieSessionFactory  # TODO: should needs to 
 #html_escape_mako = mako.filters.html_escape
 #mako.filters.html_escape = lambda s: html_escape_mako(str(s) if not isinstance(s, str) else s)
 
-# HACK! - Monkeypatch - SignedCookieSessionFactory
-import pyramid.session
-pyramid.session.manage_changed = lambda x: x
-
 
 def main(global_config, **settings):
     """
         This function returns a Pyramid WSGI application.
     """
     # Setup --------------------------------------------------------------------
-    
+
     # Db
     init_DBSession(settings)
-    
+
     # Pyramid Global Settings
     config = Configurator(settings=settings) #, autocommit=True
 
     # Register Aditional Includes ---------------------------------------------
     config.include('pyramid_mako')  # The mako.directories value is updated in the scan for addons. We trigger the import here to include the correct folders.
-    
+
     # Parse/Convert setting keys that have specifyed datatypes
     for key in config.registry.settings.keys():
         config.registry.settings[key] = convert_str_with_type(config.registry.settings[key])
@@ -61,9 +57,9 @@ def main(global_config, **settings):
     # Search Config ------------------------------------------------------------
     import karakara.views.search
     karakara.views.search.search_config = read_json(config.registry.settings['karakara.search.view.config'])
-    
+
     # WebSocket ----------------------------------------------------------------
-    
+
     def authenicator(key):
         """Only admin authenticated keys can connect to the websocket"""
         request = Request({'HTTP_COOKIE':'{0}={1}'.format(config.registry.settings['session.cookie_name'],key)})
@@ -76,9 +72,9 @@ def main(global_config, **settings):
     )
     config.registry['socket_manager'] = socket_manager
     socket_manager.start()
-    
+
     # Renderers ----------------------------------------------------------------
-    
+
     # AllanC - currently the auto_format decorator does all the formatting work
     #          it would be far preferable to use the pyramid renderer framework
     #          issue is, we want to set the renderer to be dynamic based on the url given
@@ -93,24 +89,23 @@ def main(global_config, **settings):
     # closeset ive seen
     #   http://zhuoqiang.me/a/restful-pyramid
     #   http://stackoverflow.com/questions/4633320/is-there-a-better-way-to-switch-between-html-and-json-output-in-pyramid
-    
-    
+
+
     # Routes -------------------------------------------------------------------
-    
+
     # Static Routes
     config.add_static_view(name='ext'   , path='../externals/static') #cache_max_age=3600 # settings["static.assets"]
     config.add_static_view(name='static', path='karakara:{0}'.format(settings["static.assets"])) #cache_max_age=3600 # settings["static.assets"]
     config.add_static_view(name='player', path='karakara:{0}'.format(settings["static.player"]))
-    
+
     # AllanC - it's official ... static route setup and generation is a mess in pyramid
     #config.add_static_view(name=settings["static.media" ], path="karakara:media" )
     config.add_static_view(name='files' , path=settings["static.media"])
-    
-    
+
     # Routes
     def append_format_pattern(route):
         return re.sub(r'{(.*)}', r'{\1:[^/\.]+}', route) + r'{spacer:[.]?}{format:(%s)?}' % '|'.join(registered_formats())
-    
+
     config.add_route('home'          , append_format_pattern('/')              )
     config.add_route('track'         , append_format_pattern('/track/{id}')    )
     config.add_route('track_list'    , append_format_pattern('/track_list')    )
@@ -130,18 +125,18 @@ def main(global_config, **settings):
     config.add_route('comunity_list' , append_format_pattern('/comunity/list') )
     config.add_route('comunity_track', append_format_pattern('/comunity/track/{id}'))
     config.add_route('comunity_upload', append_format_pattern('/comunity/upload'))
-    
+
     config.add_route('search_tags'   , '/search_tags/{tags:.*}')
     config.add_route('search_list'   , '/search_list/{tags:.*}')
-    
+
     # Upload extras -----
     config.add_route('upload', '/upload{sep:/?}{name:.*}')
     #config.add_route('uploaded', '/uploaded')
     config.add_static_view(name=settings['upload.url.path.uploaded'], path=settings['upload.path'])
-    
+
     # Events -------------------------------------------------------------------
     config.add_subscriber(add_template_helpers_to_event, pyramid.events.BeforeRender)
-    
+
     # Return -------------------------------------------------------------------
     config.scan(ignore='.tests')
     config.scan('externals.lib.pyramid_helpers.views')
@@ -150,5 +145,3 @@ def main(global_config, **settings):
 
 def add_template_helpers_to_event(event):
     event['h'] = template_helpers
-
-    
