12a23be07be8dd5d2bf213cdad06ce39ca8ecae9
Allan Callaghan <calaldees@gmail.com>
2014-09-22 07:43:00 +0100

    pep8'ed Comunity and considered mocking open() for testing ComunityTrack

--- website/karakara/views/comunity.py
+++ website/karakara/views/comunity.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import joinedload
 
 from externals.lib.misc import backup
 
-from ..model              import DBSession
+from ..model import DBSession
 from ..model.model_tracks import Track
 
-from . import web, action_ok, action_error, cache, generate_cache_key, comunity_only
+from . import web, action_ok, cache, generate_cache_key, comunity_only  # action_error,
 
 
 from ..scripts.import_tracks import import_json_data as import_track
@@ -35,9 +35,11 @@ def invalidate_list_cache(request=None):
     list_version += 1
     cache.delete(LIST_CACHE_KEY)
 
+
 def _generate_cache_key(request):
     global list_version
     return '-'.join([generate_cache_key(request), str(list_version)])
+
 
 #-------------------------------------------------------------------------------
 # Community Utils
@@ -55,12 +57,13 @@ class ComunityTrack():
     manipulate these files.
     """
 
-    def __init__(self, request, track_id):
+    def __init__(self, request, track_id, open=open):
         assert track_id and track_id != 'undefined', 'track_id required'
         self.media_path = request.registry.settings['static.media']
         self.track_id = track_id
         self._track_dict = None
         self._import_required = False
+        self._open = open  # Allow mockable open() for testing
 
     @property
     def track(self):
@@ -76,52 +79,62 @@ class ComunityTrack():
         return self._track_dict
 
     def import_track(self):
-        with open(self.path_description_filename, 'r') as filehandle:
+        with self._open(self.path_description_filename, 'r') as filehandle:
             import_track(filehandle, self.path_description_filename)
             invalidate_track(self.track_id)
 
     @property
     def path(self):
         return os.path.join(self.media_path, self.track['source_filename'])
+
     @property
     def path_backup(self):
         return os.path.join(self.path, '_old_versions')
+
     @property
     def path_source(self):
         return os.path.join(self.path, 'source')
+
     @property
     def path_description_filename(self):
         return os.path.join(self.path, 'description.json')
+
     @property
     def tag_data_filename(self):
         return os.path.join(self.path, 'tags.txt')
+
     @property
     def tag_data_raw(self):
-        with open(self.tag_data_filename ,'r') as tag_data_filehandle:
+        with self._open(self.tag_data_filename, 'r') as tag_data_filehandle:
             return tag_data_filehandle.read()
     @tag_data_raw.setter
     def tag_data_raw(self, tag_data):
         backup(self.tag_data_filename, self.path_backup)
-        with open(self.tag_data_filename ,'w') as filehandle:
+        with self._open(self.tag_data_filename, 'w') as filehandle:
             filehandle.write(tag_data)
             self._import_required = True
+
     @property
     def tag_data(self):
         return {tuple(line.split(':')) for line in self.tag_data_raw.split('\n')}
+
     @property
     def source_data_filename(self):
         return os.path.join(self.path, 'sources.json')
+
     @property
     def source_data(self):
-        with open(self.source_data_filename ,'r') as source_data_filehandle:
+        with self._open(self.source_data_filename, 'r') as source_data_filehandle:
             return json.load(source_data_filehandle)
+
     @property
     def subtitle_filenames(self):
         return [k for k in self.source_data.keys() if re.match(r'^.*\.(ssa|srt)$', k)]
+
     @property
     def subtitle_data(self):
         def subtitles_read(subtitle_filename):
-            with open(os.path.join(self.path_source, subtitle_filename) ,'r') as subtitle_filehandle:
+            with self._open(os.path.join(self.path_source, subtitle_filename), 'r') as subtitle_filehandle:
                 return subtitle_filehandle.read()
         return dict(((subtitle_filename, subtitles_read(subtitle_filename)) for subtitle_filename in self.subtitle_filenames))
     @subtitle_data.setter
@@ -129,12 +142,14 @@ class ComunityTrack():
         for subtitle_filename, subtitle_data_raw in subtitle_data:
             subtitle_path_filename = os.path.join(self.path_source, subtitle_filename)
             backup(subtitle_path_filename, self.path_backup)
-            with open(subtitle_path_filename, 'w') as filehandle:
+            with self._open(subtitle_path_filename, 'w') as filehandle:
                 filehandle.write(subtitle_data_raw)
                 self._import_required = True
+
     @property
     def video_previews(self):
         return h.previews(self.track)
+
 
 #-------------------------------------------------------------------------------
 # Community Views
@@ -144,6 +159,7 @@ class ComunityTrack():
 @web
 def comunity(request):
     return action_ok()
+
 
 @view_config(route_name='comunity_upload')
 @web
@@ -201,6 +217,8 @@ def comunity_track(request):
         'subtitles': ctrack.subtitle_data,
         'previews': ctrack.video_previews,
     })
+    # Todo - should throw action error with details on fail. i.e. if static files unavalable
+
 
 @view_config(route_name='comunity_track', request_method='POST')
 @web
