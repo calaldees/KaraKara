b9174f9b97debb01284e0c4615188548b41d3d3b
Allan <allan.callaghan@thisisglobal.com>
2014-11-05 07:34:00 +0000

    added 'persona' to legal providers - this should probably be a db table pep8'ted some of the file

--- website/karakara/model/model_comunity.py
+++ website/karakara/model/model_comunity.py
@@ -1,7 +1,7 @@
 from . import Base, JSONEncodedDict
 
-from sqlalchemy     import Column, Enum, ForeignKey
-from sqlalchemy     import Integer, Unicode, DateTime, Boolean
+from sqlalchemy import Column, Enum, ForeignKey
+from sqlalchemy import Integer, Unicode, DateTime, Boolean
 from sqlalchemy.orm import relationship
 
 import copy
@@ -12,39 +12,40 @@ __all__ = [
     "ComunityUser",
 ]
 
-_provider_types = Enum('facebook', 'google', 'twitter', 'janrain', 'gigya', 'test_provider', name='provider_types')
+# This is probably better as a db table
+_provider_types = Enum('facebook', 'google', 'twitter', 'janrain', 'gigya', 'persona', 'test_provider', name='provider_types')
 
 
 class ComunityUser(Base):
     """
     """
-    __tablename__   = "comunity_user"
+    __tablename__ = "comunity_user"
 
-    id              = Column(Integer(), primary_key=True)
-    name            = Column(Unicode(), nullable=True)
-    email           = Column(Unicode(), nullable=True)
-    timestamp       = Column(DateTime(), nullable=False, default=now)
-    approved        = Column(Boolean(), nullable=False, default=False)
-    
-    tokens          = relationship("SocialToken", cascade="all, delete-orphan")
-    
+    id        = Column(Integer(), primary_key=True)
+    name      = Column(Unicode(), nullable=True)
+    email     = Column(Unicode(), nullable=True)
+    timestamp = Column(DateTime(), nullable=False, default=now)
+    approved  = Column(Boolean(), nullable=False, default=False)
+
+    tokens    = relationship("SocialToken", cascade="all, delete-orphan")
+
     @property
     def user_data(self):
         if self.tokens:
             return self.tokens[0].data
         return {}
-    
+
     __to_dict__ = copy.deepcopy(Base.__to_dict__)
     __to_dict__.update({
         'default': {
-            'id'          : None ,
-            'name'        : None ,
-            'email'       : None ,
-            'timestamp'   : None ,
-            'approved'    : None ,
+            'id'       : None,
+            'name'     : None,
+            'email'    : None,
+            'timestamp': None,
+            'approved' : None,
         },
     })
-    
+
     __to_dict__.update({'full': copy.deepcopy(__to_dict__['default'])})
     __to_dict__['full'].update({
     })
@@ -54,7 +55,7 @@ class SocialToken(Base):
     """
     """
     __tablename__ = 'social_token'
-    
+
     id       = Column(Integer(), primary_key=True)
     user_id  = Column(Integer(), ForeignKey('comunity_user.id'), nullable=False)
     token    = Column(Unicode(), nullable=False, index=True)
