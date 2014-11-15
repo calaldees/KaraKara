ac5ece3395ebf314fd4e0dd77c9388751dd2acee
Allan <allan.callaghan@thisisglobal.com>
2014-11-15 19:22:08 +0000

    If logged_in and /comunity/login -> /comunity

--- website/karakara/views/comunity_login.py
+++ website/karakara/views/comunity_login.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
 from externals.lib.social.social_login import SocialLogin
 
@@ -14,10 +15,14 @@ social_login = SocialLogin()
 @view_config(route_name='comunity_logout')
 @web
 def comunity_logout(request):
+    if not request.session.get('user'):
+        raise HTTPFound(location='/comunity')
     return social_login.logout(request)
 
 
 @view_config(route_name='comunity_login')
 @web
 def comunity_login(request):  # , login_provider=login_provider, user_store=user_store
+    if request.session.get('user'):
+        raise HTTPFound(location='/comunity')
     return social_login.login(request)
