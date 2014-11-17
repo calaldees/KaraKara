e9f505a4559fa5c46fd84a1a603319654ca0c934
Allan <calaldees@gmail.com>
2014-11-17 07:28:20 +0000

    approve_user can now approve all pending users

--- website/karakara/auth/utils.py
+++ website/karakara/auth/utils.py
@@ -3,10 +3,10 @@ from ..model.model_comunity import ComunityUser
 
 
 def list_pending_users():
-    return [user.email for user in DBSession.query(ComunityUser).filter(ComunityUser.approved == False)]
+    return [user.email for user in DBSession.query(ComunityUser).filter(ComunityUser.approved==False)]
 
 
-def approve_user(email, auto_commit=True):
+def approve_user(email=None, auto_commit=True):
     """
     Example user
 
@@ -14,10 +14,15 @@ def approve_user(email, auto_commit=True):
     from karakara.auth.utils import * ; list_pending_users()
     ['...', '...']
     approve_user('user@email.com')
+    or
+    approve_user()  # for all unapproved users
 
     User will need to logout and login again once approved
     """
-    user = DBSession.query(ComunityUser).filter(ComunityUser.email == email).one()
-    user.approved = True
+    query = DBSession.query(ComunityUser)
+    if email:
+        query = query.filter(ComunityUser.email == email)
+    for user in query.all():
+        user.approved = True
     if auto_commit:
         commit()
