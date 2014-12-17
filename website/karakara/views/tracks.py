4e46f3798715253f612fc1eab32fb710332137ec
Allan Callaghan <calaldees@gmail.com>
2014-12-17 08:03:03 +0000

    pep8'ed tracks.py

--- website/karakara/views/tracks.py
+++ website/karakara/views/tracks.py
@@ -10,7 +10,7 @@ from . import web, action_ok, action_error, etag_decorator, cache, cache_none, g
 from ._logic import queue_item_for_track
 from .search import restrict_search
 
-from ..model              import DBSession
+from ..model import DBSession
 from ..model.model_tracks import Track
 #from ..model.model_queue  import QueueItem
 
@@ -42,6 +42,7 @@ def generate_cache_key_track(request):
         str(request.registry.settings.get('karakara.system.user.readonly')),
     ])
 
+
 #-------------------------------------------------------------------------------
 # Track
 #-------------------------------------------------------------------------------
@@ -52,35 +53,33 @@ def generate_cache_key_track(request):
 def track_view(request):
     """
     View individual track details
-    
+
     This method has two levels of cache:
      - track_dict cache - cache the track data (should not change)
      - track_dict + queue_dict - every time this track is modifyed in the queue it will invalidate the cache
-    
+
     The track dicts that are fetched from the DB are cached with infinate expiray
     time as they will not change once the system is running
-    
+
     It might be wise to add an cache_clear to the import_tracks operation
     (currently this wont be needed as the basic implementation is python_dict,
      which is invalidatated on shutdown,
      but if we were going to use memcache or redis then this is nessisary)
     """
     id = request.matchdict['id']
-    
+
     def get_track_dict(id):
         try:
             log.debug('cache gen - track_dict for {0}'.format(id))
-            return DBSession.query(Track) \
-                    .options( \
-                        joinedload(Track.tags), \
-                        joinedload(Track.attachments), \
-                        joinedload('tags.parent'), \
-                        joinedload('lyrics'), \
-                    ) \
-                    .get(id).to_dict('full')
+            return DBSession.query(Track).options(
+                joinedload(Track.tags),
+                joinedload(Track.attachments),
+                joinedload('tags.parent'),
+                joinedload('lyrics'),
+            ).get(id).to_dict('full')
         except AttributeError:
             return cache_none
-    
+
     def get_track_and_queued_dict(id):
         track = cache.get_or_create("track_dict:{0}".format(id), lambda: get_track_dict(id))
         if not track:
@@ -89,10 +88,10 @@ def track_view(request):
         def queue_item_list_to_dict(queue_items):
             return [queue_item.to_dict('full', exclude_fields='track_id,session_owner') for queue_item in queue_items]
         track['queue'] = queue_item_for_track(request, DBSession, track['id'])
-        track['queue']['played' ] = queue_item_list_to_dict(track['queue']['played'] )
+        track['queue']['played'] = queue_item_list_to_dict(track['queue']['played'] )
         track['queue']['pending'] = queue_item_list_to_dict(track['queue']['pending'])
         return track
-    
+
     # TODO: Put some thought into the idea that a malicious cock could deliberately
     #       perform repeated calls knowing a cache key could will be created and
     #       take the system down with an 'out of memory'. Then again, if they want to
@@ -103,7 +102,7 @@ def track_view(request):
 
     log_event(request, track_id=id)
 
-    return action_ok(data={'track' : track})
+    return action_ok(data={'track': track})
 
 
 #@view_config(route_name='track_list')
@@ -128,15 +127,14 @@ def track_list_all(request):
     Return a list of every track in the system (typically for printing)
     """
     log.info('track_list')
-    
-    tracks = DBSession.query(Track).\
-                options(\
-                    joinedload(Track.tags),\
-                    joinedload('tags.parent')\
-                )
+
+    tracks = DBSession.query(Track).options(
+        joinedload(Track.tags),
+        joinedload('tags.parent'),
+    )
     tracks = restrict_search(request, tracks)
     track_list = [track.to_dict(include_fields='tags') for track in tracks]
-    
+
     # Sort track list
     #  this needs to be handled at the python layer because the tag logic is fairly compicated
     fields = request.registry.settings.get('karakara.print_tracks.fields',[])
@@ -144,4 +142,4 @@ def track_list_all(request):
         return " ".join([tag_hireachy(track['tags'], field) for field in fields])
     track_list = sorted(track_list, key=key_track)
 
-    return action_ok(data={'list':track_list})
+    return action_ok(data={'list': track_list})
