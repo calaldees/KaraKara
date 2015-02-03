a0e50139dc0b3355941a8dd62a958b98089c2fe9
Allan Callaghan <calaldees@gmail.com>
2015-02-03 08:40:12 +0000

    Mini refactor to hash matcher to pave way for the hash mover

--- website/karakara/scripts/hash_matcher.py
+++ website/karakara/scripts/hash_matcher.py
@@ -2,6 +2,7 @@ import hashlib
 import re
 import os
 import pickle
+from functools import partial
 
 from externals.lib.misc import file_scan, update_dict
 
@@ -20,21 +21,25 @@ DEFAULT_FILE_EXTS = {'mp4', 'avi', 'rm', 'mkv', 'ogm', 'ssa', 'srt', 'ass'}
 
 # Utils ------------------------------------------------------------------------
 
-def hash_files(source_folder=None, destination_folder=None, hasher=hashlib.sha256, file_exts=DEFAULT_FILE_EXTS, **kwargs):
-    file_regex = re.compile(r'.*\.({})$'.format('|'.join(file_exts)))
-
-    gen_hashs = lambda folder: {
+def hash_files(folder, file_regex=None, hasher=hashlib.sha256):
+    return {
         f.hash: f
         for f in file_scan(folder, file_regex=file_regex, hasher=hasher)
     }
 
+
+# ------------------------------------------------------------------------------
+
+def hash_source_dest(source_folder=None, destination_folder=None, hasher=hashlib.sha256, file_exts=DEFAULT_FILE_EXTS, **kwargs):
+    file_regex = re.compile(r'.*\.({})$'.format('|'.join(file_exts)))
+    gen_hashs_folder = partial(hash_files, **dict(hasher=hasher, file_regex=file_regex))
     return {
-        'source_files': gen_hashs(source_folder),
-        'destination_files': gen_hashs(destination_folder),
+        'source_files': gen_hashs_folder(source_folder),
+        'destination_files': gen_hashs_folder(destination_folder),
     }
 
 
-def match_files(source_files=None, destination_files=None, destination_folder=None, dry_run=False, **kwargs):
+def symlink_matched_files(source_files=None, destination_files=None, destination_folder=None, dry_run=False, **kwargs):
     for key in sorted(set(source_files.keys()).difference(set(destination_files.keys())), key=lambda key: source_files[key].file):
         f = source_files[key]
         log.debug(f.file)
@@ -43,6 +48,12 @@ def match_files(source_files=None, destination_files=None, destination_folder=No
                 os.symlink(f.absolute, os.path.join(destination_folder, f.file))
             except OSError:
                 log.info('unable to symlink {0}'.format(f.file))
+
+
+# ------------------------------------------------------------------------------
+
+def move_files():
+    pass
 
 
 # Command Line -----------------------------------------------------------------
@@ -63,7 +74,7 @@ def get_args():
     parser.add_argument('-e', '--file_exts', nargs='*', help='file exts to find', default=DEFAULT_FILE_EXTS)
 
     # Operation
-    parser.add_argument('-c', '--copy', action='store_true', help='copy files to destination (to be ready for importing)', default=False)
+    #parser.add_argument('-c', '--copy', action='store_true', help='copy files to destination (to be ready for importing)', default=False)
 
     # Cache
     parser.add_argument('--cache_filename', action='store', help='', default=DEFAULT_CACHE_FILENAME)
@@ -87,10 +98,10 @@ def main():
             data = pickle.load(f)
     except IOError:
         with open(args['cache_filename'], 'wb') as f:
-            data = hash_files(**args)
+            data = hash_source_dest(**args)
             pickle.dump(data, f)
 
-    match_files(**update_dict(args.copy(), data))
+    symlink_matched_files(**update_dict(args.copy(), data))
 
 if __name__ == "__main__":
     main()
