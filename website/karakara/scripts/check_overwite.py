75f2b73b344ed8e29c2e78846ea42f6830d7c1d2
Allan Callaghan <calaldees@gmail.com>
2015-02-21 23:01:48 +0000

    check_overwrite was being venusaian scanned and running every time. Wrapped it in a __main__ block. This was REALLY sloppy and could have been really bad

--- website/karakara/scripts/check_overwite.py
+++ website/karakara/scripts/check_overwite.py
@@ -7,25 +7,30 @@ log = logging.getLogger(__name__)
 
 VERSION = "0.0"
 
-print("Detect which tags.txt have been buggered")
-help = """
-Check the mtime of the latest .bak file.
-If the tags.txt file is NEWER than then latest bak then we have pushed the wrong data and have lost info
-"""
 
-tag_file = {}
-bak_file = defaultdict(int)
+def check_tags():
+    print("Detect which tags.txt have been buggered")
+    help = """
+    Check the mtime of the latest .bak file.
+    If the tags.txt file is NEWER than then latest bak then we have pushed the wrong data and have lost info
+    """
 
-for f in file_scan('/Users/allan.callaghan/Applications/KaraKara/files/', file_regex='.*(txt|bak)$', stats=True):
-    folder = f.relative.split('/')[0]
-    mtime = f.stats.st_mtime
-    if 'tags.txt' == f.file:
-        tag_file[folder] = mtime
-    if '.bak' in f.file:
-        if mtime > bak_file[folder]:
-            bak_file[folder] = mtime
+    tag_file = {}
+    bak_file = defaultdict(int)
 
-for folder, mtime in tag_file.items():
-    if mtime < bak_file[folder]:
-        print(folder)
+    for f in file_scan('/Users/allan.callaghan/Applications/KaraKara/files/', file_regex='.*(txt|bak)$', stats=True):
+        folder = f.relative.split('/')[0]
+        mtime = f.stats.st_mtime
+        if 'tags.txt' == f.file:
+            tag_file[folder] = mtime
+        if '.bak' in f.file:
+            if mtime > bak_file[folder]:
+                bak_file[folder] = mtime
 
+    for folder, mtime in tag_file.items():
+        if mtime < bak_file[folder]:
+            print(folder)
+
+
+if __name__ == "__main__":
+    main()
