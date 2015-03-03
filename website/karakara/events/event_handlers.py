865a029c92f0a678d5751e0ee425a5dad62ce1f3
Allan Callaghan <calaldees@gmail.com>
2015-03-03 08:52:54 +0000

    Patch to prevent flooding of events (kind of crude)

--- website/karakara/events/event_handlers.py
+++ website/karakara/events/event_handlers.py
@@ -5,12 +5,22 @@ from externals.lib.pyramid_helpers.events import SessionCreated
 
 from externals.lib.log import log_event
 
+known_ip_address = set()
+
 
 @subscriber(SessionCreated)
 def session_created(event):
     """
     On first connect, log the devices user agent
     """
+    # Quick hack to prevent flooding of event logs for
+    #  - Users with cookies turned off (hopefully not to many of these in the real world).
+    #  - Test users that create a new request every time.
+    ip = event.request.environ.get('REMOTE_ADDR')
+    if ip in known_ip_address:
+        return
+    known_ip_address.add(ip)
+
     log_event(
         event.request,
         device=httpagentparser.detect(event.request.environ.get('HTTP_USER_AGENT')),
