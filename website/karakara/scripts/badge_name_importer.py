3f910ee7784535d42d59751b121811a1b11b1d6d
Allan Callaghan <calaldees@gmail.com>
2015-03-30 12:59:49 +0100

    Considered badge name importers for other events

--- website/karakara/scripts/badge_name_importer.py
+++ website/karakara/scripts/badge_name_importer.py
@@ -9,8 +9,32 @@ log = logging.getLogger(__name__)
 
 VERSION = '0.0'
 DEFAULT_CACHE_FILENAME = 'badgenames.cache.html'
-REGEX_NAME = re.compile(r'(?P<name>.*)\((?P<badge_name>.*)\)')
 
+
+# Processors -------------------------------------------------------------------
+
+def minami_processor(soup):
+    """
+    """
+    regex = re.compile(r'(?P<name>.*)\((?P<badge_name>.*)\)')
+    return [regex.match(name_string).groupdict()['badge_name'] for name_string in (cell.string for cell in soup.find_all('td')) if regex.match(name_string)]
+minami_processor.url = "http://minamicon.org.uk/members.php"
+
+
+def ame_processor(soup):
+    """
+    """
+    import pdb ; pdb.set_trace()
+    return []
+ame_processor.url = "http://www.amecon.org.uk/registration/members"
+
+processors = {
+    'ame': ame_processor,
+    'minami': minami_processor
+}
+
+
+# Comamnd Line -----------------------------------------------------------------
 
 def get_args():
     import argparse
@@ -21,7 +45,7 @@ def get_args():
         epilog=""" """
     )
 
-    parser.add_argument('-s', '--source_url', action='store', help='')
+    parser.add_argument('-s', '--source', action='store', help='', choices=processors.keys())
 
     parser.add_argument('-v', '--verbose', action='store_true', help='', default=False)
     parser.add_argument('--version', action='version', version=VERSION)
@@ -30,23 +54,27 @@ def get_args():
     return args
 
 
+# Main -------------------------------------------------------------------------
+
 def main():
     args = get_args()
     logging.basicConfig(level=logging.DEBUG if args['verbose'] else logging.INFO)
+
+    processor = processors[args['source']]
 
     response_text = ''
     if os.path.exists(DEFAULT_CACHE_FILENAME):
         with open(DEFAULT_CACHE_FILENAME, 'r') as f:
             response_text = f.read()
     else:
-        response_text = requests.get(args['source_url']).text
+        response_text = requests.get(processor.url).text
         if not os.path.exists(DEFAULT_CACHE_FILENAME):
             with open(DEFAULT_CACHE_FILENAME, 'w') as f:
                 f.write(response_text)
 
     soup = BeautifulSoup(response_text)
 
-    badge_names = [REGEX_NAME.match(name_string).groupdict()['badge_name'] for name_string in (cell.string for cell in soup.find_all('td')) if REGEX_NAME.match(name_string)]
+    badge_names = processor(soup)
 
     for badge_name in badge_names:
         print(badge_name)
