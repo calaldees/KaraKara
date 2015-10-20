df44d449445530ed617be17212061b3c7b4d4217
Allan Callaghan <calaldees@gmail.com>
2015-10-20 19:29:44 +0100

    Added abstraction for processed file destination

--- processmedia2/encode_media.py
+++ processmedia2/encode_media.py
@@ -1,12 +1,12 @@
 import os.path
 import tempfile
-import shutil
 
 from libs.misc import postmortem, hashfile, freeze, first
 from libs.file import FolderStructure
 
 from processmedia_libs import add_default_argparse_args
 from processmedia_libs.meta_manager import MetaManager, FileItemWrapper
+from processmedia_libs.processed_files_manager import ProcessedFilesManager
 from processmedia_libs import external_tools
 
 import logging
@@ -60,11 +60,10 @@ class Encoder(object):
     """
 
     def __init__(self, meta_manager=None, path_meta=None, path_processed=None, path_source=None, **kwargs):
-        self.path_processed = path_processed
-
         self.meta = meta_manager or MetaManager(path_meta)
         self.destination = FolderStructure.factory(path_processed)
         self.fileitem_wrapper = FileItemWrapper(path_source)
+        self.processed_files_manager = ProcessedFilesManager(path_processed)
 
     def _get_meta(self, name):
         self.meta.load(name)
@@ -79,57 +78,46 @@ class Encoder(object):
         self.meta.save(name)
 
     def _encode_video_from_meta(self, m):
+        # 1.) Calculate starting files and target files
         source_files = self.fileitem_wrapper.wrap_scan_data(m)
+        target_file = self.processed_files_manager.factory((f['hash'] for f in source_files.values() if f), 'mp4')
 
-        def gen_hash_string(hashs):
-            hash_str = str(frozenset(hashs).__hash__())
-            if hash_str.startswith('-'):
-                hash_str = hash_str.strip('-')+'x'
-            return hash_str
-
-        # 1.) Assertain if encoding is actually needed by hashing sources
-        source_hash = gen_hash_string(f['hash'] for f in source_files.values() if f)
-        # 1.a) and comparing input and output hashs
-        if m.processed_data.setdefault('main', {}).get('source_hash') == source_hash: # and os.path.join(self.path_processed, ):  # TODO: check the file exists in destination
+        # 2.) Assertain if encoding is actually needed by hashing sources and comparing input and output hashs
+        if m.processed_data.setdefault('main', {}).get('hash') == target_file.hash and target_file.exists:
             log.info('Processed Destination was created with the same input sources - no encoding required')
-            return True
-        # 1.b) Check if hash already exisits in destination
-        target_file = os.path.join(self.path_processed, '{}.mp4'.format(source_hash))
-        if os.path.exists(target_file) or first(self.destination.scan(lambda file_item: file_item.file_no_ext == source_hash)):
-            log.info('Processed Destination already exisits - no encoding required - meta now links to destination')
-            m.processed_data['main']['source_hash'] = source_hash
+            m.processed_data['main']['hash'] = target_file.hash
             return True
 
         with tempfile.TemporaryDirectory() as tempdir:
-            # 2.) Convert souce formats into appropriate formats for video encoding
+            # 3.) Convert souce formats into appropriate formats for video encoding
 
-            # 2.a) Convert Image to Video
+            # 3.a) Convert Image to Video
             if source_files['image'] and not source_files['video']:
                 log.warn('image to video conversion is not currently implemented')
                 return False
 
-            # 2.b) Convert srt to ssa
+            # 3.b) Convert srt to ssa
             if not source_files['sub'].get('ext') == 'ssa':
                 log.warn('convert subs to ssa no implemented yet')
                 source_files['sub'] = {'absolute': None}
                 return False
 
-            # 3.) Encode
+            # 4.) Encode
             encode_steps = (
-                # 3.a) Render video with subtitles (without audio)
+                # 4.a) Render video with subtitles (without audio)
                 lambda: external_tools.encode_video(
                     source=source_files['video']['absolute'],
                     sub=source_files['sub'].get('absolute'),
                     destination=os.path.join(tempdir, 'video.avi'),
                 ),
 
-                # 3.b) Render audio and normalize
+                # 4.b) Render audio and normalize
                 lambda: external_tools.encode_audio(
                     source=source_files['audio'].get('absolute') or source_files['video'].get('absolute'),
                     destination=os.path.join(tempdir, 'audio.wav'),
                 ),
 
-                # 3.c) Mux Video and Audio
+                # 4.c) Mux Video and Audio
                 lambda: external_tools.mux(
                     video=os.path.join(tempdir, 'video.avi'),
                     audio=os.path.join(tempdir, 'audio.wav'),
@@ -142,20 +130,17 @@ class Encoder(object):
                     import pdb ; pdb.set_trace()
                     return False
 
-            shutil.move(os.path.join(tempdir, 'mux.mp4'), target_file)
-            m.processed_data['main']['source_hash'] = source_hash
+            # 5.) Move the newly encoded file to the target path
+            target_file.move(os.path.join(tempdir, 'mux.mp4'))
+            m.processed_data['main']['hash'] = target_file.hash
             import pdb ; pdb.set_trace()
 
             print('Done')
 
-            # 4.) Moved procesed file
-            # move_to_processed(filepath, 'main')
-
-
     def encode_preview(self, name):
         log.warn('preview encoding unimplemented')
         # Generate low bitrate preview
-        # move_to_processed(filepath, 'preview')
+        #target_file = self.processed_files_manager.factory((f['hash'] for f in source_files.values() if f), 'mp4')
 
 
 
--- processmedia2/processmedia_libs/processed_files_manager.py
+++ processmedia2/processmedia_libs/processed_files_manager.py
@@ -1,17 +1,35 @@
+import os.path
+import shutil
 
 class ProcessedFilesManager(object):
 
     def __init__(self, path):
-        pass
+        self.path = path
 
-    def move_to_processed(self, filepath, file_type):
-        # m.processed_data[file_type]['hash'] = hashfile()
-        # m.processed_data[file_type]['mtime'] = stats().mtime
-        # mv filepath -> PATH_PROCESSED/hash.original_ext
-        pass
-
-    def check_file_exists(self):
-        pass
+    def factory(self, hashs, ext):
+        return ProcessedFile(self.path, hashs, ext)
 
     def prune_unnneded_files(self, hashset):
         pass
+
+
+class ProcessedFile(object):
+    def __init__(self, path, hashs, ext):
+        self.hash = self.gen_string_hash(hashs)
+        self.absolute = os.path.abspath(os.path.join(path, '{}.{}'.format(self.hash, ext)))
+
+    def move(self, source_file):
+        # m.processed_data[file_type]['hash'] = hashfile()
+        # m.processed_data[file_type]['mtime'] = stats().mtime
+        # mv filepath -> PATH_PROCESSED/hash.original_ext
+        shutil.move(source_file, self.absolute)
+
+    @property
+    def exists(self):
+        return os.path.exists(self.absolute)
+
+    def gen_string_hash(self, hashs):
+        hash_str = str(frozenset(hashs).__hash__())
+        if hash_str.startswith('-'):
+            hash_str = hash_str.strip('-')+'x'
+        return hash_str
