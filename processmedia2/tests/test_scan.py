010f5f86c69728b8e14e1de723e705dd6f3c9654
Allan Callaghan <calaldees@gmail.com>
2016-03-17 19:31:34 +0000

    Considered scan manager class

--- processmedia2/tests/test_scan.py
+++ processmedia2/tests/test_scan.py
@@ -30,20 +30,30 @@ def temp_source_folder(files):
 def scan_meta(source_files):
     with tempfile.TemporaryDirectory() as path_meta:
         with temp_source_folder(source_files) as path_source:
-            scan_media(
-                path_source=path_source,
-                path_meta=path_meta
-            )
-            meta = {}
-            for f in os.listdir(path_meta):
-                with open(os.path.join(path_meta, f), 'r') as meta_filehandle:
-                    meta[f] = json.load(meta_filehandle)
-            yield meta
+            yield ScanManager(path_source, path_meta)
+
+
+class ScanManager(object):
+
+    def __init__(self, path_source, path_meta):
+        self.path_source = path_source
+        self.path_meta = path_meta
+
+    @property
+    def meta(self):
+        scan_media(path_source=self.path_source, path_meta=self.path_meta)
+
+        meta = {}
+        for f in os.listdir(self.path_meta):
+            with open(os.path.join(self.path_meta, f), 'r') as meta_filehandle:
+                meta[f] = json.load(meta_filehandle)
+        return meta
 
 
 # Tests ------------------------------------------------------------------------
 
 def test_scan_grouping():
-    with scan_meta(TEST1_VIDEO_FILES + TEST2_AUDIO_FILES) as meta:
-        assert {k for k in meta['test1.json']['scan'].keys()} == {'test1.avi', 'test1.srt', 'test1.txt'}
-        assert {k for k in meta['test2.json']['scan'].keys()} == {'test2.ogg'}
+    with scan_meta(TEST1_VIDEO_FILES + TEST2_AUDIO_FILES) as scan:
+        meta = scan.meta
+        assert set(meta['test1.json']['scan'].keys()) == {'test1.avi', 'test1.srt', 'test1.txt'}
+        assert set(meta['test2.json']['scan'].keys()) == {'test2.ogg'}
