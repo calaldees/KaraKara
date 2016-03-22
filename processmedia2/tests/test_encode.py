a02159eb252267acd3914f60a8aa9bba69e0fbc7
Allan Callaghan <calaldees@gmail.com>
2016-03-22 17:46:27 +0000

    Check image frame colors as part of encode test

--- processmedia2/tests/test_encode.py
+++ processmedia2/tests/test_encode.py
@@ -1,12 +1,18 @@
 import pytesseract
 from PIL import Image
 
-from libs.misc import color_distance
+from libs.misc import color_distance, color_close
 from processmedia_libs.external_tools import get_frame_from_video
 from ._base import ScanManager, TEST1_VIDEO_FILES
 
 COLOR_SUBTITLE_CURRENT = (255, 255, 0)
 COLOR_SUBTITLE_NEXT = (255, 255, 255)
+
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+
+SAMPLE_COORDINATE = (100, 100)
 
 
 def test_encode_simple():
@@ -16,18 +22,24 @@ def test_encode_simple():
         processed_files = scan.processed_files('test1')
         video_file = processed_files['video'][0].absolute
 
-        assert 'Red' == read_subtitle_text(video_file, 5, COLOR_SUBTITLE_CURRENT)
-        assert 'Green' == read_subtitle_text(video_file, 5, COLOR_SUBTITLE_NEXT)
+        image = get_frame_from_video(video_file, 5)
+        assert color_close(COLOR_RED, image.getpixel(SAMPLE_COORDINATE))
+        assert 'Red' == read_subtitle_text(image, COLOR_SUBTITLE_CURRENT)
+        assert 'Green' == read_subtitle_text(image, COLOR_SUBTITLE_NEXT)
 
-        assert 'Green' == read_subtitle_text(video_file, 15, COLOR_SUBTITLE_CURRENT)
-        assert 'Blue' == read_subtitle_text(video_file, 15, COLOR_SUBTITLE_NEXT)
+        image = get_frame_from_video(video_file, 15)
+        assert color_close(COLOR_GREEN, image.getpixel(SAMPLE_COORDINATE))
+        assert 'Green' == read_subtitle_text(image, COLOR_SUBTITLE_CURRENT)
+        assert 'Blue' == read_subtitle_text(image, COLOR_SUBTITLE_NEXT)
 
-        assert 'Blue' == read_subtitle_text(video_file, 25, COLOR_SUBTITLE_CURRENT)
-        assert not read_subtitle_text(video_file, 25, COLOR_SUBTITLE_NEXT)
+        image = get_frame_from_video(video_file, 25)
+        assert color_close(COLOR_BLUE, image.getpixel(SAMPLE_COORDINATE))
+        assert 'Blue' == read_subtitle_text(image, COLOR_SUBTITLE_CURRENT)
+        assert not read_subtitle_text(image, COLOR_SUBTITLE_NEXT)
 
 
-def read_subtitle_text(video_file, time, color):
-    return pytesseract.image_to_string(extract_image_color(get_frame_from_video(video_file, time), color)).strip()
+def read_subtitle_text(image, color):
+    return pytesseract.image_to_string(extract_image_color(image, color)).strip()
 
 
 def extract_image_color(source, color, threshold=50):
