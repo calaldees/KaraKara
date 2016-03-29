0156dc482a7f988e8eca0d9ef3fe9168a6656765
Allan Callaghan <calaldees@gmail.com>
2016-03-29 08:42:34 +0100

    import test now runs (with a horific path hack)

--- processmedia2/tests/test_import.py
+++ processmedia2/tests/test_import.py
@@ -1,9 +1,10 @@
 import pytest
 
 from karakara.model.init_data import init_data
-from model.model_tracks import Track  #, Tag, Attachment, _attachment_types
+from karakara.model.model_tracks import Track  #, Tag, Attachment, _attachment_types
 
 from import_media import import_media
+
 from ._base import ScanManager, TEST1_VIDEO_FILES, TEST2_AUDIO_FILES
 
 import logging
@@ -20,13 +21,13 @@ def DBSession(request, ini_file=INI):
     from pyramid.paster import get_appsettings
     from karakara.model import DBSession, init_DBSession
     init_DBSession(get_appsettings(ini_file))
-    #init_data()
+    init_data()
     return DBSession
 
 
 def test_import_simple(DBSession):
-    with ScanManager(TEST1_VIDEO_FILES + TEST2_AUDIO_FILES) as scan:
+    with ScanManager(TEST1_VIDEO_FILES | TEST2_AUDIO_FILES) as scan:
         scan.scan_media()
         scan.encode_media()
-
+        import pdb ; pdb.set_trace()
         import_media(path_meta=scan.path_meta, path_processed=scan.path_processed)
