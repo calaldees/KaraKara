4f7b1412605ab339ed867e0c028d1160ee2b52c1
Allan Callaghan <calaldees@gmail.com>
2018-02-13 13:00:37 +0000

    Added queue_home and home tests for redirect and html links

--- website/karakara/tests/integration/test_home.py
+++ website/karakara/tests/integration/test_home.py
@@ -1,11 +1,18 @@
 
 
 def test_home(app):
-    """
-    Main Menu
-    """
     response = app.get('/')
     assert response.status_code == 200
     assert 'html' in response.content_type
-    for text in ['KaraKara', 'jquery.mobile', 'queue']:
+    for text in ['KaraKara', 'jquery.mobile', 'queue_id', 'mobile.home.input_queue_label']:
         assert text in response.text
+
+
+def test_home_queue_not_exists_redirect(app):
+    response = app.get('/?queue_id=not_a_queue')
+    assert response.status_code == 302
+    response = response.follow()
+    assert response.status_code == 302
+    response = response.follow()
+    assert 'view.queue.not_exist' in response.text
+    assert 'not_a_queue' in response.text
