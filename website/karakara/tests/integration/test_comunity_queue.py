9144869257ff8c0c860842f16e371d429c55e111
Allan Callaghan <calaldees@gmail.com>
2018-02-18 12:45:07 +0000

    Added test stub for untested feature

---
+++ website/karakara/tests/integration/test_comunity_queue.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.skip('todo')
+def test_comunity_queue(app, queue):
+    # Check queue list 403
+    # login
+    # assert contains qtest
+    # add ptest
+    # assert contains qtest and ptest
+    # del ptest
+    # assert contains quest and !ptest
+    assert False, 'unimplemented'
