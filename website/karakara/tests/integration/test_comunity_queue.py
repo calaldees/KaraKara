7cdff50557c89a9ffe552eea5d3799e23bbf4f82
Allan Callaghan <calaldees@gmail.com>
2018-02-19 07:39:06 +0000

    Added test for add/remove queues

--- website/karakara/tests/integration/test_comunity_queue.py
+++ website/karakara/tests/integration/test_comunity_queue.py
@@ -1,13 +1,27 @@
 import pytest
 
+from .test_comunity import login, logout
 
-@pytest.mark.skip('todo')
-def test_comunity_queue(app, queue):
-    # Check queue list 403
-    # login
-    # assert contains qtest
-    # add ptest
-    # assert contains qtest and ptest
-    # del ptest
-    # assert contains quest and !ptest
-    assert False, 'unimplemented'
+
+def test_comunity_queue(app, queue, users):
+    response = app.get('/comunity/queues', expect_errors=True)
+    assert response.status_code == 403
+
+    login(app)
+
+    QUEUE_NEW = 'queue_new'
+    response = app.get('/comunity/queues')
+    assert queue in response.text
+    assert QUEUE_NEW not in response.text
+
+    response = app.post('/comunity/queues', {'id': QUEUE_NEW, 'format': 'redirect'})
+    response = app.get('/comunity/queues')
+    assert queue in response.text
+    assert QUEUE_NEW in response.text
+
+    response = app.get(f'/comunity/queues?method=delete&format=redirect&queue.id={QUEUE_NEW}')
+    response = app.get('/comunity/queues')
+    assert queue in response.text
+    assert QUEUE_NEW not in response.text
+
+    logout(app)
