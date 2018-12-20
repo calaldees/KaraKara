79330c0bf3a8b0fcc216066bc854b99c7b201cb5
Allan Callaghan <calaldees@gmail.com>
2018-12-20 16:41:21 +0000

    Moved processmedia2 Dockerfile to ./processmedia2/Dockerfile to be inline with other other containers processmedia2 container now copys in code by default processmedia2 (in docker-compose.yml) no longer requires volume mount of code Removed uneeded processmedia2 dockerfiles

--- docker/processmedia2/build_processmedia2_container.bat
+++
@@ -1,8 +0,0 @@
-rem SET PATH_BUILD=..
-
-docker pull ubuntu:latest
-docker pull jrottenberg/ffmpeg:latest
-
-docker build -t karakara/processmedia2_base:latest --file .\processmedia2.base.dockerfile .
-docker build -t karakara/processmedia2:latest --file .\processmedia2.production.dockerfile %PATH_BUILD%
-REM docker push karakara/processmedia2:latest
