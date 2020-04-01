d4a25d97ee0ef0fb6eb68c8ecdea97d0bf7abec2
Allan Callaghan <calaldees@gmail.com>
2020-04-01 22:37:47 +0100

    Websocket/HTTP bridge working

--- websocket/server.py
+++ websocket/server.py
@@ -1,5 +1,10 @@
+from functools import cached_property
+from collections import defaultdict
+from itertools import chain
+from urllib.parse import parse_qsl
+
 import aiohttp
-from aiohttp import web
+from aiohttp import web, WSCloseCode
 
 import logging
 log = logging.getLogger(__name__)
@@ -8,29 +13,81 @@ log = logging.getLogger(__name__)
 class Server():
 
     def __init__(self, *args, **kwargs):
+        """
+        Websocket broadcast via http
+
+        ws://host:port/channel_name.ws
+        GET /channel_name?message=hello
+        POST /channel_name <message=hello>
+        """
         pass
 
-    @property
+    @cached_property
     def app(self):
         app = web.Application()
-        app.router.add_get('/ws', self.websocket_handler)
-        app.router.add_get("/", self.index_handler)
+
+        app['channels'] = defaultdict(set)
+        # template_lookup = aiohttp_mako.setup(
+        #     app,
+        #     directories=['.'],
+        #     input_encoding='utf-8',
+        #     output_encoding='utf-8',
+        #     default_filters=['decode.utf8'],
+        # )
+        with open('index.html', 'rt', encoding='utf-8') as filehandle:
+            self.template_index = filehandle.read()
+
+        app.router.add_get("/", self.handle_index)
+        app.router.add_get("/{channel}.ws", self.handle_channel_websocket)
+        app.router.add_route("*", "/{channel}", self.handle_channel)
+        app.on_shutdown.append(self.on_shutdown)
+
         return app
 
-    async def index_handler(self, request):
-        data = {'some': 'data'}
-        #import pdb ; pdb.set_trace()
+    async def on_shutdown(self, app):
+        for ws in tuple(chain.from_iterable(app['channels'].values())):
+            await ws.close(code=WSCloseCode.GOING_AWAY, message='shutdown')
+
+    #@aiohttp_mako.template('index.html')
+    async def handle_index(self, request):
+        if request.headers['accept'].startswith('text/html'):
+            return web.Response(text=self.template_index, content_type='text/html')
+        data = {
+            'channels': {
+                channel_name: len(clients)
+                for channel_name, clients in request.app['channels'].items()
+            },
+        }
         return web.json_response(data)
 
-    async def websocket_handler(self, request):
+    async def handle_channel(self, request):
+        channel_name = request.match_info['channel']
+        channel = request.app['channels'][channel_name]
+        data = {**dict(parse_qsl(request.query_string)), **await request.post()}
+        message = data.get('message', '')
+        for ws in channel:
+            await ws.send_str(message)
+        return web.json_response({
+            'message': message,
+            'recipients': len(channel),
+        })
+
+    async def handle_channel_websocket(self, request):
         ws = web.WebSocketResponse()
         await ws.prepare(request)
-        log.info('websocket onConnected')
-        async for msg in ws:
-            if msg.type == aiohttp.WSMsgType.ERROR:
-                log.error(ws.exception())
-            await ws.close()  # close on any message - websockets are only for listening
-        log.info('websocket onDisconnected')
+        channel_name = request.match_info['channel']
+        log.info(f'websocket onConnected {request.remote=} {channel_name=}')
+        channel = request.app['channels'][channel_name]
+        channel.add(ws)
+        try:
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.ERROR:
+                    log.error(ws.exception())
+                await ws.send_str('This service is for listening only - closing connection')
+                await ws.close()
+        finally:
+            channel.remove(ws)
+        log.info(f'websocket onDisconnected {request.remote=} {channel_name=}')
         return ws
 
 
