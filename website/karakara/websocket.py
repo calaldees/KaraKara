e12e7cb792784e0764c370024964debe74be8779
Shish <shish@shishnet.org>
2021-02-16 10:36:48 +0000

    s/channel/topic/ for consistency

--- website/karakara/websocket.py
+++ website/karakara/websocket.py
@@ -3,15 +3,15 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def _send_websocket_message(request, channel, message, retain):
+def _send_websocket_message(request, topic, message, retain):
     c = mqtt.Client()
     c.username_pw_set("karakara", "aeyGGrYJ")
     c.connect(request.queue.settings.get('karakara.websocket.host'))
     c.loop_start()
 
-    msg = c.publish("karakara/room/"+request.queue.id+"/"+channel, message, retain=retain)
+    msg = c.publish("karakara/room/"+request.queue.id+"/"+topic, message, retain=retain)
     msg.wait_for_publish()
 
-def send_websocket_message(request, channel, message, retain=False):
+def send_websocket_message(request, topic, message, retain=False):
     """Proxy for unittests mocking"""
-    _send_websocket_message(request, channel, message, retain)
+    _send_websocket_message(request, topic, message, retain)
