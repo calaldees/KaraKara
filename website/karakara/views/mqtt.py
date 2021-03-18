4f53ddcf1ccd35ccabae620c6a6515e1b061ff4c
Shish <shish@shishnet.org>
2021-03-18 16:07:37 +0000

    have karakara webserver be the source of truth for mqtt auth

---
+++ website/karakara/views/mqtt.py
@@ -0,0 +1,53 @@
+from pyramid.view import view_config
+
+from . import action_ok, action_error
+
+from ..model import DBSession
+from ..model.model_queue import QueueSetting
+
+
+def user(request):
+    _ = request.translate
+    username: str = request.params.get("username")
+    password: str = request.params.get("password")
+
+    queue_setting_password = DBSession.query(QueueSetting).filter(
+        QueueSetting.queue_id == username, QueueSetting.key == 'karakara.private.password'
+    ).one()
+
+    # Not a real user, used for unit tests
+    if username == "test" and password == "test":
+        return action_ok()
+
+    if password == queue_setting_password.value:
+        return action_ok()
+    else:
+        raise action_error(code=403)
+
+
+def acl(request):
+    _ = request.translate
+    clientid: str = request.params.get("clientid")
+    username: str = request.params.get("username")
+    topic: str = request.params.get("topic")
+    access: str = request.params.get("access")
+
+    # KaraKara is the admin user who can read/write everything
+    if username == "karakara":
+        return action_ok()
+    
+    # Special cases for unit tests
+    if topic.startswith("test/public/"):
+        return action_ok()
+    if username == "test" and topic.startswith("test/private/"):
+        return action_ok()
+
+    # Each room can write to its own topics
+    if topic.startswith("karakara/room/{username}/"):
+        return action_ok()
+    
+    # Everybody can subscribe to room state broadcasts
+    if topic.startswith("karakara/room/") and access == "read":
+        return action_ok()
+
+    raise action_error(code=403)
