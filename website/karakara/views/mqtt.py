1e2d1ca9e3977996c95d10df58f68fd61ce49621
Shish <shish@shishnet.org>
2021-03-19 14:17:44 +0000

    fixes for mqtt auth views

--- website/karakara/views/mqtt.py
+++ website/karakara/views/mqtt.py
@@ -5,7 +5,13 @@ from . import action_ok, action_error
 from ..model import DBSession
 from ..model.model_queue import QueueSetting
 
+import logging
+log = logging.getLogger(__name__)
 
+
+@view_config(
+    context='karakara.traversal.MQTTUserContext',
+)
 def user(request):
     _ = request.translate
     username: str = request.params.get("username")
@@ -13,24 +19,33 @@ def user(request):
 
     queue_setting_password = DBSession.query(QueueSetting).filter(
         QueueSetting.queue_id == username, QueueSetting.key == 'karakara.private.password'
-    ).one()
+    ).first()
 
     log.info(f'trying to mqtt-auth as user {username}')
 
     # Not a real user, used for unit tests
     if username == "test" and password == "test":
-        return action_ok()
+        return action_ok(message="test ok")
 
     # Admin user not associated with any particular queue
     if username == "karakara" and password == "aeyGGrYJ":
-        return action_ok()
+        return action_ok(message="admin ok")
+
+    if not password:
+        raise action_error(message="no password specified", code=403)
+
+    if not queue_setting_password:
+        raise action_error(message="queue has no password", code=403)
 
     if password == queue_setting_password.value:
-        return action_ok()
-    else:
-        raise action_error(code=403)
+        return action_ok(message="queue owner ok")
+
+    raise action_error(message="login failed", code=403)
 
 
+@view_config(
+    context='karakara.traversal.MQTTAclContext',
+)
 def acl(request):
     _ = request.translate
     clientid: str = request.params.get("clientid")
@@ -42,20 +57,20 @@ def acl(request):
 
     # KaraKara is the admin user who can read/write everything
     if username == "karakara":
-        return action_ok()
+        return action_ok(message="admin ok")
 
     # Special cases for unit tests
     if topic.startswith("test/public/"):
-        return action_ok()
+        return action_ok(message="public test ok")
     if username == "test" and topic.startswith("test/private/"):
-        return action_ok()
+        return action_ok(message="private test ok")
 
     # Each room can write to its own topics
     if topic.startswith("karakara/room/{username}/"):
-        return action_ok()
+        return action_ok(message="queue owner ok")
 
     # Everybody can subscribe to room state broadcasts
     if topic.startswith("karakara/room/") and access == "read":
-        return action_ok()
+        return action_ok(message="anon read ok")
 
-    raise action_error(code=403)
+    raise action_error(message="login failed", code=403)
