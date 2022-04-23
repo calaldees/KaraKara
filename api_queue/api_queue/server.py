24cf53ba36267b3f196005e4c00c47c83e42b7f0
Allan Callaghan <calaldees@gmail.com>
2022-04-23 14:41:16 +0100

    sanic-redis now works tests fail because they require the container - consider mocks?

--- api_queue/api_queue/server.py
+++ api_queue/api_queue/server.py
@@ -1,11 +1,42 @@
 from sanic import Sanic, response
+from sanic_redis import SanicRedis
 
 app = Sanic("karakara_queue")
 #app.ctx.db = Database()
 
+
+app.config.update(
+    {
+        'REDIS': "redis://redis:6379/0",
+    }
+)
+
+redis = SanicRedis(config_name="REDIS")
+redis.init_app(app)
+
+# >>> import redis
+# >>> r = redis.Redis(host='localhost', port=6379, db=0)
+# >>> r.set('foo', 'bar')
+# True
+# >>> r.get('foo')
+# b'bar'
+
+# from redis import asyncio as aioredis
+# async def main():
+#     redis = aioredis.from_url("redis://localhost")
+#     await redis.set("my-key", "value")
+#     value = await redis.get("my-key")
+#     print(value)
+
+
 @app.get("/")
-def basic(request):
-    return response.text("foo")
+async def basic(request):
+    #return response.text("foo")
+    r = request.app.ctx.redis
+    await r.set('key2', 'value2')
+    result = await r.get('key2')
+    return response.text(str(result))
+
 
 
 if __name__ == '__main__':
