de60c9ae03e58fab5581059c5a3dcef8d5e1d9be
Shish <shish@shishnet.org>
2022-09-17 05:01:26 +0100

    [api_queue] add type hints, fix all the things that mypy complains about, fixes #115

--- api_queue/api_queue/queue_model.py
+++ api_queue/api_queue/queue_model.py
@@ -1,6 +1,6 @@
 import dataclasses
 import datetime
-from typing import Iterator, Optional
+import typing as t
 import random
 import numbers
 from functools import reduce
@@ -15,7 +15,7 @@ class QueueItem():
     track_duration: datetime.timedelta
     session_id: str
     performer_name: str
-    start_time: Optional[datetime.datetime] = None
+    start_time: t.Optional[datetime.datetime] = None
     id: int = dataclasses.field(default_factory=lambda:random.randint(0,2**30))
 
     def __post_init__(self):
@@ -57,9 +57,10 @@ class QueueItem():
         return dict(map(_parse, key_values))
 
     @property
-    def end_time(self) -> datetime.datetime:
+    def end_time(self) -> t.Optional[datetime.datetime]:
         if self.start_time:
             return self.start_time + self.track_duration
+        return None
 
 
 class Queue():
@@ -67,71 +68,78 @@ class Queue():
         self.items = items
         self.track_space = settings["track_space"]
         self.modified = False
-        self._now: Optional[datetime.datetime] = None
+        self._now: t.Optional[datetime.datetime] = None
     @property
     def now(self) -> datetime.datetime:
         return self._now or datetime.datetime.now()
     @property
-    def past(self) -> Iterator[QueueItem]:
+    def past(self) -> t.Iterable[QueueItem]:
         now = self.now
-        return (i for i in self.items if i.start_time and i.end_time < now)
+        return (i for i in self.items if i.end_time and i.end_time < now)
     @property
-    def future(self) -> Iterator[QueueItem]:
+    def future(self) -> t.Iterable[QueueItem]:
         now = self.now
         return (i for i in self.items if not i.start_time or i.start_time >= now)
     @property
-    def current(self) -> QueueItem:
-        return self.playing or next(self.future, None)
+    def current(self) -> t.Optional[QueueItem]:
+        return self.playing or next(iter(self.future), None)
     @property
-    def current_future(self) -> Iterator[QueueItem]:
+    def current_future(self) -> t.Iterable[QueueItem]:
         # fugly mess - you can do this in less lines
         current = self.current
         future = list(self.future)
-        if future and future[0] != current:
+        if current and future and future[0] != current:
             future.insert(0, current)
         return future
     @property
-    def last(self) -> QueueItem:
+    def last(self) -> t.Optional[QueueItem]:
         return self.items[-1] if self.items else None
     @property
-    def playing(self) -> QueueItem:
+    def playing(self) -> t.Optional[QueueItem]:
         now = self.now
-        return next((i for i in self.items if i.start_time and i.start_time <= now and i.end_time > now), None)
+        return next((i for i in self.items if i.start_time and i.start_time <= now and i.end_time and i.end_time > now), None)
     @property
     def is_playing(self) -> bool:
-        return self.current and self.current.start_time
+        return bool(self.current and self.current.start_time)
     @property
     def end_time(self) -> datetime.datetime:
-        if self.last.end_time:
+        if self.last and self.last.end_time:
             return self.last.end_time
         def track_duration_reducer(end_time, i):
             end_time += i.track_duration + self.track_space
             return end_time
         return reduce(track_duration_reducer, self.future, self.now)
     def play(self) -> None:
-        self.current.start_time = self.now
-        self._recalculate_start_times()
+        if self.current:
+            self.current.start_time = self.now
+            self._recalculate_start_times()
     def stop(self) -> None:
-        self.current.start_time = None
-        self._recalculate_start_times()
+        if self.current:
+            self.current.start_time = None
+            self._recalculate_start_times()
     def add(self, queue_item: QueueItem) -> None:
         self.items.append(queue_item)
         self._recalculate_start_times()
     def move(self, id1: int, id2: int) -> None:
         assert {id1, id2} < {i.id for i in self.current_future}|{-1,}, 'move track_ids are not in future track list'
         index1, queue_item = self.get(id1)
+        assert index1 is not None
+        assert queue_item is not None
         del self.items[index1]
         index2, _ = self.get(id2) if id2 != -1 else (len(self.items), None) # id's are positive. `-1` is a special sentinel value for end of list
+        assert index2 is not None
         queue_item.start_time = None
         self.items.insert(index2, queue_item)
         self._recalculate_start_times()
-    def get(self, id: int) -> tuple[int, QueueItem]:
+    def get(self, id: int) -> t.Union[t.Tuple[int, QueueItem], t.Tuple[None, None]]:
         return next(((index, queue_item) for index, queue_item in enumerate(self.items) if queue_item.id==id), (None, None))
     def delete(self, id: int) -> None:
         now = self.now
         self.items[:] = [i for i in self.items if (i.start_time and i.start_time < now) or i.id != id]
         self._recalculate_start_times()
     def seek_forwards(self, seconds: float=20) -> None:
+        if not self.current or not self.current.start_time or not self.current.end_time:
+            return
         delta = datetime.timedelta(seconds=seconds)
         # if we're in a gap, seek stops at the end of the gap
         if self.current.start_time > self.now:
@@ -150,6 +158,8 @@ class Queue():
                 self.current.start_time -= delta
         self._recalculate_start_times()
     def seek_backwards(self, seconds: float=20) -> None:
+        if not self.current or not self.current.start_time or not self.current.end_time:
+            return
         delta = datetime.timedelta(seconds=seconds)
         # if we're in a gap, seek stops at the start of the gap
         if self.current.start_time > self.now:
@@ -165,7 +175,8 @@ class Queue():
                 self.current.start_time += delta
         self._recalculate_start_times()
     def skip(self) -> None:
-        self.delete(self.current.id)
+        if self.current:
+            self.delete(self.current.id)
     def _recalculate_start_times(self) -> None:
         for i_prev, i_next in pairwise(self.current_future):
             i_next.start_time = i_prev.end_time + self.track_space if i_prev and i_prev.start_time else None
