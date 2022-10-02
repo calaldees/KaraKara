5dd11d5957286bb0314b1c1711b963784310da67
Shish <shish@shishnet.org>
2022-10-02 13:50:34 +0100

    [cypress] browser2 bookmark test

---
+++ test_cypress/cypress/browser2/bookmark.cy.js
@@ -0,0 +1,25 @@
+describe('Bookmarks', () => {
+  beforeEach(function () {
+    cy.intercept('GET', '/files/tracks.json', { fixture: 'tracks.json' })
+    cy.visit('/browser2/')
+    cy.get("input").eq(0).type("demo")
+    cy.contains("Enter Room").click()
+  })
+  it('Add, check, remove, check', () => {
+    // Add
+    cy.contains("jdrama").click()
+    cy.contains("Birth").click()
+    cy.contains("Bookmark").click()
+    // Check that it was added
+    cy.contains('[data-cy="submit"]').click()
+    cy.contains('[data-cy="submit"]').click()
+    cy.contains("Bookmarks")
+    cy.contains("Birth").click()
+    // Remove
+    cy.contains("Un-Bookmark").click()
+    cy.contains('[data-cy="submit"]').click()
+    // Check that it was removed
+    cy.contains("Bookmarks").should('not.exist')
+    cy.contains("Birth").should('not.exist')
+  })
+})
