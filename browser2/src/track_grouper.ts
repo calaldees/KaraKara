fa29d1368e9140e7600b4dc18ac6307f8f8dafbf
Shish <shish@shishnet.org>
2022-11-04 17:57:03 +0000

    [browser2] add a ton of comments

--- browser2/src/track_grouper.ts
+++ browser2/src/track_grouper.ts
@@ -12,7 +12,7 @@ import { normalise_cmp, normalise_name } from "./utils";
  *                     from (gundam, macross, one piece)
  *   from:gundam    -> gundam (wing, waltz, unicorn)
  */
- export function suggest_next_filters(
+export function suggest_next_filters(
     filters: Array<string>,
     groups: Dictionary<Dictionary<number>>
 ): Array<string> {
@@ -106,6 +106,12 @@ export function summarise_tags(
  * - A list of tracks
  * - A list of filters
  * - A list of filter groups
+ * 
+ * Terminology for this function:
+ * - tag_key, eg "vocaltrack", "from"
+ * - tag_values, eg {"on": 32, "off": 64}, {"Gundam": 41, "Macross": 52}
+ * - tag_children, eg ["on", "off"], ["Gundam", "Macross"]
+ * - tag_child, eg "on", "Gundam"
  */
 export function group_tracks(
     filters: Array<string>,
@@ -120,75 +126,97 @@ export function group_tracks(
         sections.push(["No Results", {}]);
     }
 
-    // If we have a few tracks, just list them all, under headings if we have any
+    // If we have a few tracks, just list them all
     else if (tracks.length < 15) {
-        // If we can't figure out any sensible headings,
-        // everything will just end up in "leftover tracks"
-        let section_names = suggest_next_filters(filters, all_tags);
         let found_track_ids: Array<string> = [];
-        for(let i=0; i<section_names.length; i++) {
-            let tag_key = section_names[i]; // eg "vocaltrack"
-            let tag_values = all_tags[tag_key]; // eg {"on": 2003, "off": 255}
-            if (!tag_values) continue;
-            let tag_children = Object.keys(tag_values).sort(normalise_cmp);
-            for(let j=0; j<tag_children.length; j++) {
-                let tag_child = tag_children[j];
+        // Use suggest_next_filters to figure out some headings, eg
+        // if we are currently searching for "from:Macross" then it
+        // will suggest filtering by "Macross:*"
+        let next_filters = suggest_next_filters(filters, all_tags);
+        // If suggest_next_filters can't come up with any suggestions,
+        // or all of the suggestions are bad (eg it suggests a tag
+        // which would have 0 results due to the "if user searches for
+        // X then suggest Y next" feature) then we'll end up not having
+        // any sections, and everything will end up in leftover_tracks,
+        // which is fine.
+        let tag_keys = next_filters.filter(tag_key => tag_key in all_tags);
+        // tag_keys lists all of the top-level suggested filters, eg
+        // in this example we would just have ["Macross"]
+        tag_keys.forEach(function (tag_key) {
+            // Look at our all_tags table to see what children we have
+            let tag_children = Object.keys(all_tags[tag_key]).sort(normalise_cmp);
+            // tag_children then lists the children of this tag
+            // like ["Delta", "Frontier", "Plus"], and we want to
+            // add one [section title, track list] pair into the
+            // sections list for each of those.
+            tag_children.forEach(function (tag_child) {
                 let tracks_in_this_section = tracks.filter(t => t.tags[tag_key]?.includes(tag_child));
                 sections.push([tag_child, { tracks: tracks_in_this_section }]);
                 found_track_ids = found_track_ids.concat(tracks_in_this_section.map(t => t.id));
-            }
-        }
+            });
+        });
         leftover_tracks = leftover_tracks.filter(t => !found_track_ids.includes(t.id))
     }
 
     // If we have many tracks, prompt for more filters
     else {
-        // If we can't figure out any sensible next filters,
-        // try to group by every available tag
-        let section_names = suggest_next_filters(filters, all_tags);
-        if(section_names.length == 0) section_names = Object.keys(all_tags);
-        for (let i = 0; i < section_names.length; i++) {
-            let tag_key = section_names[i]; // eg "vocaltrack"
-            let tag_values = all_tags[tag_key]; // eg {"on": 2003, "off": 255}
-            if (!tag_values) continue;
+        // Try to pick some smart filters, like if we've searched
+        // "category:anime", then next filter by "from:*"
+        let next_filters = suggest_next_filters(filters, all_tags);
+        // If we have a ton of tracks, but we can't figure out any
+        // sensible filters, then desperately try using any and every
+        // available tag as a filter, because we really don't want to
+        // be listing 50+ tracks with no filters at all.
+        if (next_filters.length == 0) next_filters = Object.keys(all_tags);
+        // Remove any suggestions which would give 0 results
+        let tag_keys = next_filters.filter(tag_key => tag_key in all_tags);
+        // For each section, either add a filter list, or a grouped filter list
+        tag_keys.forEach(function(tag_key) {
             // If we've fallen back to using "all tags" as sections,
             // then "all tags" includes top-level tags (with null as a
             // parent) - let's turn null into something visible so that
             // top-level tags will be grouped under this.
             let tag_key_visible = tag_key || "*";
-            // If a filter has a lot of options (eg artist=...) then show options
-            // grouped alphabetically
-            if (Object.keys(tag_values).length > 50) {
+            // Look at our all_tags table to see what children we have
+            let tag_children = Object.keys(all_tags[tag_key]).sort(normalise_cmp);
+            // If a tag has a lot of children (eg artist=...) then show
+            // children grouped alphabetically
+            if (tag_children.length > 50) {
                 let grouped_groups = {};
-                Object.keys(tag_values).forEach(function (x) {
+                tag_children.forEach(function (tag_child) {
                     // Group by first alphabetic character
-                    var initial = normalise_name(x)[0];
+                    var initial = normalise_name(tag_child)[0];
                     if (grouped_groups[initial] == undefined)
                         grouped_groups[initial] = {};
-                    grouped_groups[initial][x] = tag_values[x];
-                    // If our title is "The X", also group us under X
-                    if(x.toLowerCase().startsWith("the ") && x.length > 4) {
-                        var x2 = x.substring(4) + ", " + x.substring(0, 3);
+                    grouped_groups[initial][tag_child] = all_tags[tag_key][tag_child];
+                    // If our title is "The X", then put the track into both
+                    // "T" and "X" sections in case people are looking for
+                    // "The X" or "X, The"
+                    if (tag_child.toLowerCase().startsWith("the ") && tag_child.length > 4) {
+                        var x2 = tag_child.substring(4) + ", " + tag_child.substring(0, 3);
                         var initial = x2[0].toUpperCase();
                         if (grouped_groups[initial] == undefined)
                             grouped_groups[initial] = {};
-                        grouped_groups[initial][x2] = tag_values[x];
+                        grouped_groups[initial][x2] = all_tags[tag_key][tag_child];
                     }
                 });
                 sections.push([tag_key_visible, { groups: grouped_groups }]);
             }
-            // If a filter has a few options (eg vocaltrack=on/off), then show
-            // all the options
+            // If a tag has a few children (eg vocaltrack=on/off),
+            // then show all the children
             else {
-                sections.push([tag_key_visible, { filters: tag_values }]);
+                sections.push([tag_key_visible, { filters: all_tags[tag_key] }]);
             }
             // Remove all tracks which would be discovered by this sub-query
             leftover_tracks = leftover_tracks.filter((t) => !(tag_key in t.tags));
-        }
+        });
     }
 
-    // If there are tracks which wouldn't be found if we used any
-    // of the above filters, show them in a general "Tracks" section
+    // If we came up with a bunch of sections, but not every track
+    // is included in a section, then include the leftovers in a
+    // "Tracks" section.
+    // If we didn't come up with any sections, then include the
+    // leftovers in an untitled section.
     if (leftover_tracks.length > 0) {
         let leftover_title = sections.length > 0 ? "Tracks" : "";
         sections.push([leftover_title, { tracks: leftover_tracks }]);
