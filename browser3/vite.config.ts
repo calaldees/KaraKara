5a823d5d183c791ad21ac8446700432aff236ec9
Shish <shish@shishnet.org>
2023-04-03 18:40:17 +0100

    [br3] more manifest bits

--- browser3/vite.config.ts
+++ browser3/vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      injectRegister: null, manifest: {
+        name: 'KaraKara',
+        short_name: 'KaraKara',
+        description: 'KaraKara Song Browser',
+        theme_color: '#e9e9e9',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }
+    })
+  ],
 })
