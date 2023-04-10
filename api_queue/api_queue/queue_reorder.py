6beee2a4bbfb28540d29feec638ad57ca1af3e0e
Allan Callaghan <calaldees@gmail.com>
2023-04-10 17:31:06 +0100

    first rank order test passed

--- api_queue/api_queue/queue_reorder.py
+++ api_queue/api_queue/queue_reorder.py
@@ -17,7 +17,7 @@ def _rank(queue: Queue, queue_item: QueueItem) -> Number:
     def _minuets(t: datetime.timedelta):
         return t.total_seconds()/60 if t else None
     def _minuets_ago(d: datetime.datetime):
-        return _minuets(queue._now - d) if d else None
+        return _minuets(queue.now - d) if d else None
     def _hours_ago(d: datetime.datetime):
         return _minuets_ago(d)/60 if d else None
 
@@ -36,7 +36,7 @@ def _rank(queue: Queue, queue_item: QueueItem) -> Number:
     ))
 
     def _sang_ago_score(i: QueueItem):
-        sang_hours_ago = _minuets_ago(i.start_time)  # the start_time could be hypothetical (e.g: it has not been sung, but is scheduled/estimated to be start_time)
+        sang_hours_ago = _hours_ago(i.start_time)  # the start_time could be hypothetical (e.g: it has not been sung, but is scheduled/estimated to be start_time)
         if sang_hours_ago:
             # if you sang a single 4 miuet song 1 hour ago
             # added_minuets_ago will rise linearly with time while this rank decays with time
@@ -45,9 +45,13 @@ def _rank(queue: Queue, queue_item: QueueItem) -> Number:
         return _minuets(i.track_duration) * 2  # need thought - it's in the queue, but has no start_time, so the queue is currently paused  # the queue could be reordered, so we apply a static penalty  # I don't even know if this is needed
     sang_ago_penalty = sum(map(_sang_ago_score, queued))
 
+    #print(f'{queue_item.track_id=}: {sang_ago_penalty=} - {added_minuets_ago=}')
     return sang_ago_penalty - added_minuets_ago
 
 
 def reorder(queue: Queue):
-    reorder_index = queue.items.index(queue.current) + queue.settings["coming_soon_track_count"]
+    queue_item_current = queue.current
+    if not queue_item_current:
+        return
+    reorder_index = queue.items.index(queue_item_current)-1 + queue.settings["coming_soon_track_count"]
     queue.items[reorder_index:] = sorted(queue.items[reorder_index:], key=partial(_rank, queue))
--- api_queue/tests/test_queue_reorder.py
+++ api_queue/tests/test_queue_reorder.py
@@ -1,8 +1,9 @@
+import pytest
 from unittest.mock import patch
 import datetime
 
 from api_queue.queue_model import QueueItem
-from api_queue.queue_reorder import reorder
+from api_queue.queue_reorder import reorder, _rank
 
 
 def _QueueItem(*args, added_time=datetime.datetime(2022, 1, 1, 10, 0, 0), **kwargs):
@@ -14,17 +15,41 @@ def _mins_ago(minutes):
     return datetime.datetime.now() - datetime.timedelta(minutes=minutes)
 
 
+@pytest.fixture
+def qu(qu):
+    qu._now = None
+    qu.settings["coming_soon_track_count"] = 3
+    return qu
+
+
 def test_empty_reorder(qu):
-    assert reorder(qu) is None
+    reorder(qu)
+
+def test_no_reorder__tracks_within_coming_soon_track_count(qu):
+    qu.add(QueueItem('Track1', 60, 'TestSession1', 'test_name1', added_time=_mins_ago(60)))
+    qu.add(QueueItem('Track2', 60, 'TestSession2', 'test_name2', added_time=_mins_ago(50)))
+    qu.add(QueueItem('Track3', 60, 'TestSession3', 'test_name3', added_time=_mins_ago(40)))
+    reorder(qu)
+    assert [i.track_id for i in qu.future] == ['Track1','Track2','Track3']
 
 def test_no_reorder(qu):
-    qu.add(QueueItem('Track1', 60, 'TestSession1', 'test_name1', added_time=_mins_ago(20)))
-    qu.add(QueueItem('Track2', 60, 'TestSession2', 'test_name2', added_time=_mins_ago(15)))
-    qu.add(QueueItem('Track3', 60, 'TestSession3', 'test_name3', added_time=_mins_ago(10)))
+    qu.add(QueueItem('Track1', 60, 'TestSession1', 'test_name1', added_time=_mins_ago(60)))
+    qu.add(QueueItem('Track2', 60, 'TestSession2', 'test_name2', added_time=_mins_ago(50)))
+    qu.add(QueueItem('Track3', 60, 'TestSession3', 'test_name3', added_time=_mins_ago(40)))
+    qu.add(QueueItem('Track4', 60, 'TestSession1', 'test_name4', added_time=_mins_ago(30)))
+    qu.add(QueueItem('Track5', 60, 'TestSession2', 'test_name5', added_time=_mins_ago(20)))
+    qu.add(QueueItem('Track6', 60, 'TestSession3', 'test_name6', added_time=_mins_ago(10)))
+    reorder(qu)
+    assert [i.track_id for i in qu.future] == ['Track1','Track2','Track3','Track4','Track5','Track6']
 
-    #with patch('api_queue.queue_reorder.datetime') as mock_datetime:
-    #    mock_datetime.datetime.now.return_value = datetime.datetime(2022, 1, 1, 10, 0, 0)
-    assert reorder(qu) is None
 
-    assert [i.track_id for i in qu.future] == ['Track1','Track2','Track3']
+def test_reorder_1(qu):
+    qu.add(QueueItem('Track1', 60, 'TestSession1', 'test_name1', added_time=_mins_ago(60), start_time=_mins_ago(10)))
+    qu.add(QueueItem('Track2', 60, 'TestSession2', 'test_name2', added_time=_mins_ago(50), start_time=_mins_ago( 5)))
+    qu.add(QueueItem('Track3', 60, 'TestSession3', 'test_name3', added_time=_mins_ago(40)))
+    qu.add(QueueItem('Track4', 60, 'TestSession1', 'test_name4', added_time=_mins_ago(30)))
+    qu.add(QueueItem('Track11', 60, 'TestSession1', 'test_name1', added_time=_mins_ago(20)))  # test_name1 has sung before and should be outranked by all the others
+    qu.add(QueueItem('Track5', 60, 'TestSession3', 'test_name5', added_time=_mins_ago(10)))
+    reorder(qu)
+    assert [i.track_id for i in qu.future] == ['Track3','Track4','Track5','Track11']
 
