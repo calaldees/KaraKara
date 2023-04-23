20d9f620a0911c24db096ac9af5f37264b7f15a9
Shish <shish@shishnet.org>
2023-04-24 00:38:01 +0100

    [pl3] globe splash

--- player3/src/screens/title.tsx
+++ player3/src/screens/title.tsx
@@ -1,9 +1,14 @@
-import { useContext, useMemo } from "react";
+import { Suspense, useContext, useMemo, useRef } from "react";
 import { attachment_path } from "../utils";
 import { EventInfo, JoinInfo } from "./_common";
 import { RoomContext } from "../providers/room";
 import { ServerContext } from "../providers/server";
 import { ClientContext } from "../providers/client";
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { OrbitControls, Html, useVideoTexture, useTexture } from '@react-three/drei'
+import { Group, Object3D, TextureLoader } from "three";
+import * as THREE from "three";
+import world from '../static/world.svg';
 
 // eslint-disable-next-line
 function StatsTable() {
@@ -35,6 +40,52 @@ function StatsTable() {
     );
 }
 
+///////////////////////////////////////////////////////////////////////
+// Common
+
+export function TitleScreen() {
+    const { settings } = useContext(RoomContext);
+    let splash = null;
+    switch (settings['splash']) {
+        case "globe":
+        default:
+            splash = <Globe />;
+            break;
+        case "waterfall":
+            splash = <Waterfall />;
+            break;
+    }
+
+    return (
+        <section key="title" className={"screen_title"}>
+            {splash}
+            <JoinInfo />
+            <EventInfo />
+        </section>
+    );
+}
+
+function StatsTable({ tracks }: { tracks: Record<string, Track> }) {
+    const stats = useMemo(() => {
+        const ts = Object.values(tracks);
+        return {
+            tracks: ts.length,
+            lines: ts.map(t => t.lyrics.length).reduce((sum, n) => sum + n, 0),
+            shows: new Set(ts.map(t => t.tags.from?.[0])).size,
+            hours: Math.floor(ts.map(t => t.duration).reduce((sum, n) => sum + n, 0) / 60 / 60),
+        }
+    }, [tracks]);
+    return <h2>
+        <table>
+            <tbody>
+                {Object.entries(stats).map(([key, value]) =>
+                    <tr key={key}><th><strong>{value}</strong></th><td>{key}</td></tr>
+                )}
+            </tbody>
+        </table>
+    </h2>;
+}
+
 function getNiceTracks(tracks: Record<string, Track>, n: number) {
     const good_tracks = [
         "07_Ghost_OP1_Aka_no_Kakera",
@@ -53,6 +104,9 @@ function getNiceTracks(tracks: Record<string, Track>, n: number) {
         .slice(0, n);
     return [...good_tracks, ...first_tracks].slice(0, n);
 }
+
+///////////////////////////////////////////////////////////////////////
+// Waterfall
 
 function Waterfall() {
     const { settings } = useContext(RoomContext);
@@ -80,14 +134,100 @@ function Waterfall() {
     );
 }
 
-export function TitleScreen() {
-    let splash = null;
-    if (true) splash = <Waterfall />;
+///////////////////////////////////////////////////////////////////////
+// Globe
+
+function VideoMaterial({ url }: { url: string }) {
+    const texture = useVideoTexture(url)
+    return <meshBasicMaterial map={texture} toneMapped={false} />
+}
+
+function FallbackMaterial({ url }: { url: string }) {
+    const texture = useTexture(url)
+    return <meshBasicMaterial map={texture} toneMapped={false} side={THREE.DoubleSide} />
+}
+
+function MyScene() {
+    const { root } = useContext(ClientContext);
+    const { settings } = useContext(RoomContext);
+    const { tracks } = useContext(ServerContext);
+
+    const globe = useRef<Group>(null);
+    const text1 = useRef<Group>(null);
+    const text2 = useRef<Group>(null);
+    const colorMap = useLoader(TextureLoader, world);
+    const videos = false;
+
+    const thumbs = useMemo(() => {
+        return getNiceTracks(tracks, 20).map((track) => [
+            attachment_path(root, track.attachments.image[1]),
+            attachment_path(root, track.attachments.preview[0])
+        ]);
+    }, [tracks])
+
+    useFrame(({ clock }) => {
+        const t = clock.getElapsedTime()
+        globe.current && (globe.current.rotation.y = t / 8)
+        text1.current && text1.current.lookAt(0, Math.sin(1.5 * t), Math.sin(t));
+        text2.current && text2.current.lookAt(0, -Math.sin(1.5 * t), Math.sin(t));
+        //console.log(a) // the value will be 0 at scene initialization and grow each frame
+    })
+
+    return <>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="#29EDF2" position={[-4, 2, 5]} />
+        <group position={[3, -0.25, 0]} rotation={[0, 0, -0.15]}>
+            <group ref={globe}>
+                <mesh>
+                    <sphereGeometry args={[2.99, 20, 20]} />
+                    <meshStandardMaterial
+                        args={[{ flatShading: false }]}
+                        color={"#074D5E"}
+                        metalness={.5}
+                        roughness={.5}
+                    />
+                </mesh>
+                <mesh>
+                    <sphereGeometry args={[3, 20, 20]} />
+                    <meshStandardMaterial
+                        args={[{ flatShading: false }]}
+                        map={colorMap}
+                        color={"#29EDF2"}
+                        transparent={true}
+                    />
+                </mesh>
+                {thumbs.map(([thumb, vid], n) =>
+                    <group key={n} rotation={[0, 0.314 * n, 0]}>
+                        <mesh position={[0, 0, 3.5]}>
+                            <planeGeometry args={[1, 0.75]} />
+                            {videos ?
+                                <Suspense fallback={<FallbackMaterial url={thumb} />}>
+                                    <VideoMaterial url={vid} />
+                                </Suspense> :
+                                <FallbackMaterial url={thumb} />
+                            }
+                        </mesh>
+                    </group>
+                )}
+            </group>
+        </group>
+        <group ref={text1} position={[-4, 2.5, -5]} onUpdate={self => self.lookAt(0, 0, 0)}>
+            <Html transform>
+                <h1>{settings["title"]}</h1>
+            </Html>
+        </group>
+        <group ref={text2} position={[-4, -1.5, -5]}>
+            <Html transform><StatsTable tracks={tracks} /></Html>
+        </group>
+    </>
+}
+
+function Globe() {
     return (
-        <section key="title" className={"screen_title"}>
-            {splash}
-            <JoinInfo />
-            <EventInfo />
-        </section>
+        <div id={"splash"}>
+            <Canvas>
+                <MyScene />
+            </Canvas>
+        </div>
     );
 }
