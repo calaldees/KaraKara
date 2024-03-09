3901737c7a6d22d9cf2836f36df1945b658a3d22
Shish <shish@shishnet.org>
2024-03-09 12:47:13 +0000

    remove browser2 / player2 / processmedia2
    browser3 / player3 / processmedia3 are all confirmed as working stable enough for minami, and the old versions aren't maintained so going back to them would be dangerous

--- browser2/src/static/icons/build.sh
+++
@@ -1,4 +0,0 @@
-npx @svgr/cli --icon 1em . --out-dir . --typescript
-sed -i '' 's/import \* as React from "react"/import h from "hyperapp-jsx-pragma"/' *.tsx
-sed -i '' 's/import { SVGProps } from "react";//' *.tsx
-# rename 's/js/tsx/' *.js
