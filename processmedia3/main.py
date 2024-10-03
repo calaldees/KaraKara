947ca5edfe4d41ce534b0de5bbf19fbe1797205d
Shish <shish@shishnet.org>
2024-10-03 13:45:40 +0100

    [pm3] test-encode command to encode a single file

--- processmedia3/main.py
+++ processmedia3/main.py
@@ -312,7 +312,7 @@ def parse_args(argv: List[str]) -> argparse.Namespace:
         "cmd",
         default="all",
         nargs="?",
-        help="Sub-process to run (view, encode, export, lint, cleanup)",
+        help="Sub-process to run (view, encode, test-encode, export, lint, cleanup)",
     )
     parser.add_argument(
         "match", nargs="?", help="Only act upon files matching this pattern"
@@ -347,6 +347,25 @@ def main(argv: List[str]) -> int:
         logging_config = json.load(f)
         logging_config["root"]["level"] = logging.DEBUG if args.debug else logging.INFO
         logging.config.dictConfig(logging_config)
+
+    if args.cmd == "test-encode":
+        with logging_redirect_tqdm():
+            cache = {}
+            original = Path(args.match)
+            tracks = [Track(
+                original.parent,
+                original.stem,
+                [
+                    Source(original.parent, original, cache)
+                ],
+                [
+                    TargetType.VIDEO_H264,
+                    TargetType.VIDEO_AV1,
+                    TargetType.VIDEO_H265,
+                ]
+            )]
+            encode(tracks, args.reencode, args.threads)
+            return 0
 
     while True:
         with _pickled_var(args.processed / "cache.db", {}) as cache, logging_redirect_tqdm():
