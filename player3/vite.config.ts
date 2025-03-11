02e801d79b2787dca4ff1ecf0ebe75d9d58c6b6f
Shish <shish@shishnet.org>
2025-03-11 17:25:54 +0000

    [pl3] auto-format whole project, not just *.tsx

--- player3/vite.config.ts
+++ player3/vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslint(),
-  ],
-})
+    plugins: [react(), eslint()],
+});
