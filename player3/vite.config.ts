848fd96bc56ad5c12d7c1e8414a6ae24d3719a57
Shish <shish@shishnet.org>
2025-03-11 17:28:41 +0000

    [pl3] vite/react major updates

--- player3/vite.config.ts
+++ player3/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+    build: {
+        target: ["es2015", "ios11"],
+    },
     plugins: [react(), eslint()],
 });
