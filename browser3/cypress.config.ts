8d040513419480364304895daddd4aa54e4c4a73
Shish <shish@shishnet.org>
2025-03-11 17:22:10 +0000

    [br3] auto-format whole project, not just *.tsx

--- browser3/cypress.config.ts
+++ browser3/cypress.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    specPattern: "**/*.cye.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:1236",
-  },
-
-  component: {
-    specPattern: "**/*.cyc.{js,jsx,ts,tsx}",
-    devServer: {
-      framework: "react",
-      bundler: "vite",
+    e2e: {
+        specPattern: "**/*.cye.{js,jsx,ts,tsx}",
+        baseUrl: "http://localhost:1236",
     },
-  },
+
+    component: {
+        specPattern: "**/*.cyc.{js,jsx,ts,tsx}",
+        devServer: {
+            framework: "react",
+            bundler: "vite",
+        },
+    },
 });
