76b2cd32ef985da7d5828c2d297e0b4208c02335
Shish <shish@shishnet.org>
2025-03-12 15:19:31 +0000

    [br3] vite/eslint major updates

--- browser3/vite.config.ts
+++ browser3/vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import { VitePWA } from "vite-plugin-pwa";
-import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
     build: {
         target: ["es2015", "ios11"],
@@ -12,7 +11,6 @@ export default defineConfig({
     plugins: [
         react(),
         svgr(),
-        eslint(),
         VitePWA({
             injectRegister: null,
             manifest: {
