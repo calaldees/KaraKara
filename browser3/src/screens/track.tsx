abb89d368c17c8c4b84eea493d01ef962b91e65c
Shish <shish@shishnet.org>
2025-09-18 18:17:09 +0100

    [br3,pl3] only use text/vtt files as HTML video subtitles
    In the future we might want other subtitles for other purposes (eg, JSON subtitles for custom rendering)

--- browser3/src/screens/track.tsx
+++ browser3/src/screens/track.tsx
@@ -155,7 +155,7 @@ export function TrackDetails(): React.ReactElement {
                         type={a.mime}
                     />
                 ))}
-                {track.attachments.subtitle?.map((a) => (
+                {track.attachments.subtitle?.filter((a) => a.mime === "text/vtt").map((a) => (
                     <track
                         key={a.path}
                         src={attachment_path(root, a)}
