bcb6aceef3e7c3b007a770c500a52a22f252174f
Shish <shish@shishnet.org>
2025-09-23 16:21:20 +0100

    Back out "[pl3] vite -> rsbuild"
    Original commit changeset: 350221130b34

---
+++ player3/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+    build: {
+        target: ["es2015", "ios11"],
+        // Initial load time doesn't matter for Player, loading
+        // everything up-front for stability is more important.
+        chunkSizeWarningLimit: 2000,
+    },
+    plugins: [react()],
+});
