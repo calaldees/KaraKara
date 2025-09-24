5d60057455ef86726fb3fb194046bae20fd16bb3
Shish <shish@shishnet.org>
2025-09-24 19:52:36 +0100

    [pl3] cleaner join info

--- player3/src/components/joininfo.tsx
+++ player3/src/components/joininfo.tsx
@@ -2,11 +2,10 @@ import { useParams } from "react-router-dom";
 
 export function JoinInfo() {
     const { roomName } = useParams();
-    const root = window.location.protocol + "//" + window.location.host;
     return (
         <div id="join_info">
             <span>
-                Join at <strong>{root.replace("https://", "")}</strong> - Room
+                Join at <strong>{window.location.hostname}</strong> - Room
                 Name is <strong>{roomName}</strong>
             </span>
         </div>
