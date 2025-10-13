f4054cd89e61dfda8433399a5697c9cc36f538fa
Shish <shish@shishnet.org>
2025-10-13 15:00:42 +0100

    [pm3] delete 'contact' tag, that's only for admins to see

--- processmedia3/lib/track.py
+++ processmedia3/lib/track.py
@@ -99,6 +99,8 @@ class Track:
 
         tag_files = self._sources_by_type({SourceType.TAGS})
         tags: dict[str, list[str]] = copy.deepcopy(tag_files[0].tags)
+        if tags.get("contact"):
+            del tags["contact"]
         if tags.get("title") is None:
             raise TrackValidationException("missing tags.title")
         if tags.get("category") is None:
