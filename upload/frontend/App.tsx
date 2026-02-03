318a26d899044d1214eac9b4a2492cc7c727d363
Shish <shish@shishnet.org>
2026-02-03 10:33:29 +0000

    [upl] client-side routing

--- upload/frontend/App.tsx
+++ upload/frontend/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Index } from "./screens/index";
 
 const App = () => {
-    return <Index />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
