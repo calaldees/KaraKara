3b862acd323ee728eee26f74bb90fefab0ff787a
Shish <shish@shishnet.org>
2026-02-18 18:49:47 +0000

    [scripts] shebangs

--- scripts/badge_name_importer_minami_api.py
+++ scripts/badge_name_importer_minami_api.py
@@ -1,21 +1,22 @@
+#!/usr/bin/env python3
 import json
-import urllib.request
 import re
+import urllib.request
 
-BADGENAME_REGEX = re.compile(r'(?P<name>.*)\((?P<badge_name>.+)\)')
-url = 'http://minamicon.org.uk/php/members-list-api.php'
+BADGENAME_REGEX = re.compile(r"(?P<name>.*)\((?P<badge_name>.+)\)")
+url = "http://minamicon.org.uk/php/members-list-api.php"
 
 
 def main():
     webURL = urllib.request.urlopen(url)
     raw_data = webURL.read()
-    encoding = webURL.info().get_content_charset('utf-8')
+    encoding = webURL.info().get_content_charset("utf-8")
     data = json.loads(raw_data.decode(encoding))
 
     def get_badgename(name_string):
-        return BADGENAME_REGEX.match(name_string).groupdict().get('badge_name')
+        return BADGENAME_REGEX.match(name_string).groupdict().get("badge_name")
 
-    for badgename in (get_badgename(member_dict.get('name')) for member_dict in data):
+    for badgename in (get_badgename(member_dict.get("name")) for member_dict in data):
         print(badgename)
 
 
