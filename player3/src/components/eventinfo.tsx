de477ae2720807fbd1cb858671bda55d0b01b346
Shish <shish@shishnet.org>
2026-03-14 10:51:09 +0000

    [pl3] show a reminder to hit the play icon when needed

--- player3/src/components/eventinfo.tsx
+++ player3/src/components/eventinfo.tsx
@@ -4,37 +4,20 @@ import { RoomContext } from "@/providers/room";
 import { short_date } from "@/utils";
 
 export function EventInfo() {
-    const { settings } = useContext(RoomContext);
+    const { settings, queue } = useContext(RoomContext);
+    const nextTrackStartTime = queue[0]?.start_time;
     return (
         <div id="event_info" key={"event_info"}>
-            {settings["validation_event_end_datetime"] && (
+            {settings["validation_event_end_datetime"] ? (
                 <span>
                     Event ends at{" "}
                     <strong>
                         {short_date(settings["validation_event_end_datetime"])}
                     </strong>
                 </span>
-            )}
-            {settings["validation_event_end_datetime"] &&
-                settings["admin_list"]?.length > 0 &&
-                " - "}
-            {settings["admin_list"]?.length > 0 && (
-                <span>
-                    Admins are{" "}
-                    {settings["admin_list"].map(
-                        (a: string, n: number, as: string[]) => (
-                            <span key={a}>
-                                <strong>{a}</strong>
-                                {n === as.length - 1
-                                    ? ""
-                                    : n === as.length - 2
-                                      ? " and "
-                                      : ", "}
-                            </span>
-                        ),
-                    )}
-                </span>
-            )}
+            ) : !nextTrackStartTime ? (
+                <span>Click the <strong>play</strong> icon on the control panel when you're ready to start</span>
+            ) : null}
         </div>
     );
 }
