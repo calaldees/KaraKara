c5abf823ddc636920f894a7c7d9a5caed0353183
Shish <shish@shishnet.org>
2026-03-14 14:48:21 +0000

    [pl3] shorter "play" instruction, to fit on one line

--- player3/src/components/eventinfo.tsx
+++ player3/src/components/eventinfo.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
+import { FAIcon } from "@shish2k/react-faicon";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 import { RoomContext } from "@/providers/room";
 import { short_date } from "@/utils";
 
 export function EventInfo() {
     const { settings, queue } = useContext(RoomContext);
-    const nextTrackStartTime = queue[0]?.start_time;
     return (
         <div id="event_info" key={"event_info"}>
             {settings["validation_event_end_datetime"] ? (
@@ -15,8 +16,8 @@ export function EventInfo() {
                         {short_date(settings["validation_event_end_datetime"])}
                     </strong>
                 </span>
-            ) : !nextTrackStartTime ? (
-                <span>Click the <strong>play</strong> icon on the control panel when you're ready to start</span>
+            ) : (queue[0] && queue[0].start_time === null) ? (
+                <span>Click <strong><FAIcon icon={faPlay} /></strong> on the control panel to start</span>
             ) : null}
         </div>
     );
