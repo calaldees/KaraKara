328eb247c0b2a9a6ec58c60638c3d4aab85a900e
Shish <shish@shishnet.org>
2026-03-25 22:47:11 +0000

    [br3] global suspense handler and chunk error handler

--- browser3/src/index.tsx
+++ browser3/src/index.tsx
@@ -1,6 +1,6 @@
 /// <reference types='./index.d.ts'/>
 
-import { StrictMode } from "react";
+import { Component, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 
 import { ClientProvider } from "./providers/client";
@@ -12,15 +12,38 @@ import "./static/forms.scss";
 import "./static/layout.scss";
 import "./static/vars.scss";
 
+// Reload the page if a chunk fails to load, which can happen when
+// a new version is deployed while the user has the app open
+class ChunkErrorBoundary extends Component<{ children: React.ReactNode }> {
+    componentDidCatch(error: Error) {
+        if (
+            error?.name === "ChunkLoadError" ||
+            /Loading chunk \d+ failed/i.test(error?.message)
+        ) {
+            window.location.reload();
+        } else {
+            throw error;
+        }
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
 const root = createRoot(document.getElementById("root")!);
 root.render(
     <StrictMode>
-        <ClientProvider>
-            <TimeProvider>
-                <ServerProvider>
-                    <Root />
-                </ServerProvider>
-            </TimeProvider>
-        </ClientProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+            <ChunkErrorBoundary>
+                <ClientProvider>
+                    <TimeProvider>
+                        <ServerProvider>
+                            <Root />
+                        </ServerProvider>
+                    </TimeProvider>
+                </ClientProvider>
+            </ChunkErrorBoundary>
+        </Suspense>
     </StrictMode>,
 );
